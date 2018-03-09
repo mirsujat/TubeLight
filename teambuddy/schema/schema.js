2e9a188303cdd0f711e6fb94f3749b8252f1455d
mirsujat <mir.sujat@gmail.com>
2018-03-09 20:15:01 +0600

    RootQuery Added
    RootQuery Added and first schema done> Mir sujat

--- teambuddy/schema/schema.js
+++ teambuddy/schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 }
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -16,7 +22,14 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     user: {
       type: UserType,
-      args: { id: { type: GraphQLString } }
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
     }
   }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
--- teambuddy/server.js
+++ teambuddy/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use(
   '/graphql',
   expressGraphQL({
+    schema,
     graphiql: true
   })
 );
