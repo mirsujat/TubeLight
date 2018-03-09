01ea681ecfeacf3ab0257671cc9216d5e6500e4e
mirsujat <mir.sujat@gmail.com>
2018-03-09 19:30:42 +0600

    mir sujat
    test route done > mir sujat

--- teambuddy/server.js
+++ teambuddy/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    graphiql: true
+  })
+);
 
 app.listen(4400, () => {
   console.log('Server listening on port: 4400');
