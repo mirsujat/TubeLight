8630ebcdb80fd55b606404b86125f108b76d8446
mirsujat <mir.sujat@gmail.com>
2018-03-09 19:53:39 +0600

    GraphQL UserType schema added
     UserType schema added in GraphQL > Mir Sujat

---
+++ teambuddy/schema/schema.js
@@ -0,0 +1,12 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
