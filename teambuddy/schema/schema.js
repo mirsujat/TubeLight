143ca7b86a43f297586b52e7699e0a4f471e0e61
mirsujat <mir.sujat@gmail.com>
2018-03-10 16:25:24 +0600

    remove hard coded data and add json server
    remove hard coded data and add json server > Mir Sujat

--- teambuddy/schema/schema.js
+++ teambuddy/schema/schema.js
@@ -1,12 +1,7 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 }
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -24,7 +19,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data);
       }
     }
   }
