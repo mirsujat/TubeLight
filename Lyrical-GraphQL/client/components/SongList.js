dd6e1fd196723865d862783a87c96ed10c3b7a42
mirsujat <mir.sujat@gmail.com>
2018-03-11 20:00:58 +0600

    Adding Materialize style

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -5,7 +5,11 @@ import { graphql } from 'react-apollo';
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map(song => {
-      return <li key={song.id}>{song.title}</li>;
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
     });
   }
 
@@ -13,7 +17,7 @@ class SongList extends Component {
     if (this.props.data.loading) {
       return <div>Loading...</div>;
     }
-    return <div>{this.renderSongs()}</div>;
+    return <ul className="collection">{this.renderSongs()}</ul>;
   }
 }
 
