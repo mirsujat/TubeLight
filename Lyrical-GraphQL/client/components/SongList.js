e29e4a490c8485da6aa496835ad257f08eac85dc
mirsujat <mir.sujat@gmail.com>
2018-03-11 19:54:32 +0600

    Showing song list in the client

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return <li>{song.title}</li>;
+    });
+  }
+
   render() {
-    return <div>SongList</div>;
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.renderSongs()}</div>;
   }
 }
 
-export default SongList;
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
