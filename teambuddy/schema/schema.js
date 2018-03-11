813131f24fbfcc59dd271d5f37c919ecdc88c267
mirsujat <mir.sujat@gmail.com>
2018-03-11 10:19:20 +0600

    Bidirectional Query relation Implemented
    Bidirectional Query relation Implemented > MIr Sujat

--- teambuddy/schema/schema.js
+++ teambuddy/schema/schema.js
@@ -1,14 +1,22 @@
 const graphql = require('graphql');
 const axios = require('axios');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
   fields: {
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(resp => resp.data);
+      }
+    }
   }
 });
 
