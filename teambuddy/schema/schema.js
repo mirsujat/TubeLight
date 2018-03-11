55af810b5e3862a8937abc634cab41fa83499f59
mirsujat <mir.sujat@gmail.com>
2018-03-11 12:29:24 +0600

    Add "editUser" Mutation
    Now user can edit the user

--- teambuddy/schema/schema.js
+++ teambuddy/schema/schema.js
@@ -86,6 +86,18 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, { id }) {
         return axios.delete(`http://localhost:3000/users/${id}`).then(resp => resp.data);
       }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args).then(resp => resp.data);
+      }
     }
   }
 });
