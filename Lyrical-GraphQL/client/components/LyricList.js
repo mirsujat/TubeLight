bf096bd28b3fffac495f7ffec3d993082f289c8d
mirsujat <mir.sujat@gmail.com>
2018-03-13 12:46:44 +0600

    Showing likes with lyrics

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
   onLike(id) {
-    console.log(id);
+    this.props.mutate({ variables: { id } });
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i className="material-icons" onClick={() => this.onLike(id)}>
-            thumb_up
-          </i>
+          <div className="vote-box">
+            <i className="material-icons" onClick={() => this.onLike(id)}>
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
@@ -23,4 +28,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
