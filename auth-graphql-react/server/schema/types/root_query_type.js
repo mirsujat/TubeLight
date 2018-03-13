4264eee2ab52ea1d2e0f934340e970ca5860a79b
mirsujat <mir.sujat@gmail.com>
2018-03-13 20:17:54 +0600

    root_query updated to check authenticate status

--- auth-graphql-react/server/schema/types/root_query_type.js
+++ auth-graphql-react/server/schema/types/root_query_type.js
@@ -1,10 +1,14 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID }
+    user: { type: UserType },
+    resolve(parentValue, args, req) {
+      return req.user;
+    }
   }
 });
 
