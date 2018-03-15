7e0516b861f5424c341f502508a2d98991001bc4
mirsujat <mir.sujat@gmail.com>
2018-03-15 19:35:20 +0600

    Errors handling on Signup

--- auth-graphql-react/client/components/SignupForm.js
+++ auth-graphql-react/client/components/SignupForm.js
@@ -4,17 +4,31 @@ import { graphql } from 'react-apollo';
 import mutation from '../mutations/Signup';
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
   onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { eamil, password }
-    });
+    this.props
+      .mutate({
+        variables: { eamil, password }
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(error => error.message);
+        this.setState({ errors });
+      });
   }
 
   render() {
     return (
       <div>
         <h3>Sign Up</h3>
-        <AuthForm errors={[]} onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
       </div>
     );
   }
