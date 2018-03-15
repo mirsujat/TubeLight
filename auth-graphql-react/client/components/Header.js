de372e3b8276db9d45a3f82650198117ac4e1565
mirsujat <mir.sujat@gmail.com>
2018-03-15 11:22:27 +0600

    Implement Logout mutation

--- auth-graphql-react/client/components/Header.js
+++ auth-graphql-react/client/components/Header.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutation({});
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
 
@@ -11,7 +16,11 @@ class Header extends Component {
       return <div>Loading...</div>;
     }
     if (user) {
-      return <div>Logout</div>;
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      );
     } else {
       return (
         <div>
@@ -40,4 +49,4 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
