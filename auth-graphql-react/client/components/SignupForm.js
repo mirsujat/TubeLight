d71ebc9aa97270df66fc25927833c434a6a2cabc
mirsujat <mir.sujat@gmail.com>
2018-03-15 19:08:29 +0600

    Signup mutation Hook up

--- auth-graphql-react/client/components/SignupForm.js
+++ auth-graphql-react/client/components/SignupForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import AuthForm from './AuthForm';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Signup';
 
 class SignupForm extends Component {
   render() {
@@ -12,4 +14,4 @@ class SignupForm extends Component {
   }
 }
 
-export default SignupForm;
+export default graphql(mutation)(SignupForm);
