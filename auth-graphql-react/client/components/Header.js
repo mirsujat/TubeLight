3ee503bd2adc2acdc19166ece73458a7b52cf116
mirsujat <mir.sujat@gmail.com>
2018-03-15 10:56:03 +0600

    Setup Authentication state

--- auth-graphql-react/client/components/Header.js
+++ auth-graphql-react/client/components/Header.js
@@ -3,8 +3,25 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return <div>You are not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">{this.renderButtons()}</div>
+      </nav>
+    );
   }
 }
 
