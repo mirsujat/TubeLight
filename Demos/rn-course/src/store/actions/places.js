dc2406d30e06ba1b9acb6679179aa0e09ce7dc3a
mirsujat <mir.sujat@gmail.com>
2018-04-18 16:55:16 +0600

    firebase added to the project

--- Demos/rn-course/src/store/actions/places.js
+++ Demos/rn-course/src/store/actions/places.js
@@ -1,11 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch('https://awesome-place-1523875087723.firebaseio.com/places.json', {
+      method: 'POST',
+      body: JSON.stringify(placeData)
+    })
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      });
   };
 };
 
