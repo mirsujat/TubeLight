6575383c697cf6796274328ae14c8345534b7908
mirsujat <mir.sujat@gmail.com>
2018-04-18 13:59:59 +0600

    mirsujat

---
+++ Demos/tech_stack/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ Demos/tech_stack/src/components/ListItem.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1, color: '#575C54', paddingLeft: 10, paddingRight: 10 }}>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
+  render() {
+    const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+
+    return (
+      <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+        <View>
+          <CardSection key={this.props.library.id}>
+            <Text style={titleStyle}>{title}</Text>
+          </CardSection>
+
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15,
+    color: '#575C54'
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
