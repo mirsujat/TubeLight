dac7f408d19103b8f25a8b2dcacd619ecbcb559b
mirsujat <mir.sujat@gmail.com>
2018-04-21 16:15:13 +0600

    mirsujat

--- Demos/rn-course/src/store/actions/places.js
+++ Demos/rn-course/src/store/actions/places.js
@@ -6,6 +6,20 @@ export const addPlace = (placeName, location, image) => {
       name: placeName,
       location: location
     };
+    fetch(
+      'https://us-central1-awesome-place-1523875087723.cloudfunctions.net/storeImage',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          image: image.base64
+        })
+      }
+    )
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      });
     // fetch('https://awesome-place-1523875087723.firebaseio.com/places.json', {
     //   method: 'POST',
     //   body: JSON.stringify(placeData)
