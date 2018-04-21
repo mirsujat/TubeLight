902b2c52ee06686dc26b76b98745b3230ac56d27
mirsujat <mir.sujat@gmail.com>
2018-04-21 19:40:22 +0600

    Added new action types

--- Demos/rn-course/src/store/actions/actionTypes.js
+++ Demos/rn-course/src/store/actions/actionTypes.js
@@ -2,3 +2,6 @@ export const ADD_PLACE = 'ADD_PLACE';
 export const DELETE_PLACE = 'DELETE_PLACE';
 
 export const TRY_AUTH = 'TRY_AUTH';
+
+export const UI_START_LOADING = 'UI_START_LOADING';
+export const UI_STOP_LOADING = 'UI_STOP_LOADING';
