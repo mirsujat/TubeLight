3960ba1955ffa9faf8a9bc3df9bed72a15b8969b
mirsujat <mir.sujat@gmail.com>
2018-04-21 19:59:30 +0600

    Activity Indicator Added

--- Demos/rn-course/src/store/configureStore.js
+++ Demos/rn-course/src/store/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import placesReducer from './reducers/places';
+import uiReducer from './reducers/ui';
 
 const rootReducer = combineReducers({
-  places: placesReducer
+  places: placesReducer,
+  ui: uiReducer
 });
 
 let composeEnhancers = compose;
