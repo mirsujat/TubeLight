8e458a2723b078c84eb8cb720601779b201ff1c1
mirsujat <mir.sujat@gmail.com>
2018-04-21 14:21:43 +0600

    Test firebase deploy success

--- Demos/rn-course/src/store/actions/places.js
+++ Demos/rn-course/src/store/actions/places.js
@@ -6,15 +6,15 @@ export const addPlace = (placeName, location, image) => {
       name: placeName,
       location: location
     };
-    fetch('https://awesome-place-1523875087723.firebaseio.com/places.json', {
-      method: 'POST',
-      body: JSON.stringify(placeData)
-    })
-      .catch(err => console.log(err))
-      .then(res => res.json())
-      .then(parsedRes => {
-        console.log(parsedRes);
-      });
+    // fetch('https://awesome-place-1523875087723.firebaseio.com/places.json', {
+    //   method: 'POST',
+    //   body: JSON.stringify(placeData)
+    // })
+    //   .catch(err => console.log(err))
+    //   .then(res => res.json())
+    //   .then(parsedRes => {
+    //     console.log(parsedRes);
+    //   });
   };
 };
 
