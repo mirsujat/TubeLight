4948bc71412cd699207df9a15f960669f325134e
mirsujat <mir.sujat@gmail.com>
2018-04-21 19:37:19 +0600

    upload image to firebase DB

--- Demos/rn-course/src/store/actions/places.js
+++ Demos/rn-course/src/store/actions/places.js
@@ -2,10 +2,6 @@ import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location: location
-    };
     fetch(
       'https://us-central1-awesome-place-1523875087723.cloudfunctions.net/storeImage',
       {
@@ -18,17 +14,24 @@ export const addPlace = (placeName, location, image) => {
       .catch(err => console.log(err))
       .then(res => res.json())
       .then(parsedRes => {
+        const placeData = {
+          name: placeName,
+          location: location,
+          image: parsedRes.imageUrl
+        };
+        return fetch(
+          'https://awesome-place-1523875087723.firebaseio.com/places.json',
+          {
+            method: 'POST',
+            body: JSON.stringify(placeData)
+          }
+        );
+      })
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
         console.log(parsedRes);
       });
-    // fetch('https://awesome-place-1523875087723.firebaseio.com/places.json', {
-    //   method: 'POST',
-    //   body: JSON.stringify(placeData)
-    // })
-    //   .catch(err => console.log(err))
-    //   .then(res => res.json())
-    //   .then(parsedRes => {
-    //     console.log(parsedRes);
-    //   });
   };
 };
 
