d257ce4d03edbd82053d24323764087db4ce1154
mirsujat <mir.sujat@gmail.com>
2018-04-22 12:32:02 +0600

    mirsujat

--- Demos/rn-course/src/store/actions/places.js
+++ Demos/rn-course/src/store/actions/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+import { SET_PLACES } from './actionTypes';
 import { uiStartLoading, uiStopLoading } from './index';
 
 export const addPlace = (placeName, location, image) => {
@@ -43,6 +43,35 @@ export const addPlace = (placeName, location, image) => {
         console.log(parsedRes);
         dispatch(uiStopLoading());
       });
+  };
+};
+
+export const getPlaces = () => {
+  return dispatch => {
+    fetch('https://awesome-place-1523875087723.firebaseio.com/places.json')
+      .catch(err => {
+        alert('Something went wrong, sorry :/');
+        console.log(err);
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        const places = [];
+        for (let key in parsedRes) {
+          places.push({
+            ...parsedRes[key],
+            image: { uri: parsedRes[key].image },
+            key: key
+          });
+        }
+        dispatch(setPlaces(places));
+      });
+  };
+};
+
+export const setPlaces = places => {
+  return {
+    type: SET_PLACES,
+    places: places
   };
 };
 
