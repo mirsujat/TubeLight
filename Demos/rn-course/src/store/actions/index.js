c3dd78c5d8fc8114b8c3d2ee628225815d631718
mirsujat <mir.sujat@gmail.com>
2018-04-22 12:31:25 +0600

    mirsujat

--- Demos/rn-course/src/store/actions/index.js
+++ Demos/rn-course/src/store/actions/index.js
@@ -1,3 +1,3 @@
-export { addPlace, deletePlace, selectedPlace, deselectPlace } from './places';
+export { addPlace, deletePlace, getPlaces } from './places';
 export { tryAuth } from './auth';
 export { uiStartLoading, uiStopLoading } from './ui';
