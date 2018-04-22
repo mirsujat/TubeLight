0f2659e56c9377f4d69d615b983dea839f924574
mirsujat <mir.sujat@gmail.com>
2018-04-22 15:40:06 +0600

    test sign up success

--- Demos/rn-course/src/store/actions/auth.js
+++ Demos/rn-course/src/store/actions/auth.js
@@ -1,8 +1,34 @@
 import { TRY_AUTH } from './actionTypes';
 
 export const tryAuth = authData => {
-  return {
-    type: TRY_AUTH,
-    authData: authData
+  return dispatch => {
+    dispatch(authSignup(authData));
+  };
+};
+
+export const authSignup = authData => {
+  return dispatch => {
+    fetch(
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDBufdkcujuiUF9NGCGaHTpF5VkDqholr0',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+      .catch(err => {
+        console.log(err);
+        alert('Authentication failed, please try again!!');
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      });
   };
 };
