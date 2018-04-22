1d2d54c765960133713c1a3170d596f11cbff1d4
mirsujat <mir.sujat@gmail.com>
2018-04-22 12:32:45 +0600

    mirsujat

--- Demos/rn-course/src/store/reducers/places.js
+++ Demos/rn-course/src/store/reducers/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE, DELETE_PLACE } from '../actions/actionTypes';
+import { SET_PLACES } from '../actions/actionTypes';
 
 const initialState = {
   places: []
@@ -6,26 +6,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_PLACE:
+    case SET_PLACES:
       return {
         ...state,
-        places: state.places.concat({
-          key: Math.random(),
-          name: action.placeName,
-          image: {
-            uri: action.image.uri
-          },
-          location: action.location
-        })
+        places: action.places
       };
-
-    case DELETE_PLACE:
-      return {
-        ...state,
-        places: state.places.filter(place => {
-          return place.key !== action.placeKey;
-        })
-      };
+    // case DELETE_PLACE:
+    //   return {
+    //     ...state,
+    //     places: state.places.filter(place => {
+    //       return place.key !== action.placeKey;
+    //     })
+    //   };
 
     default:
       return state;
