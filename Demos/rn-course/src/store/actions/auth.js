ac30e9034cbbcd7d77ac296df08a57dd5e6d389d
mirsujat <mir.sujat@gmail.com>
2018-04-23 12:17:02 +0600

    mirsujat

--- Demos/rn-course/src/store/actions/auth.js
+++ Demos/rn-course/src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import { TRY_AUTH } from './actionTypes';
+import { TRY_AUTH, AUTH_SET_TOKEN } from './actionTypes';
 import { uiStartLoading, uiStopLoading } from './index';
 import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
@@ -34,11 +34,21 @@ export const tryAuth = (authData, authMode) => {
       .then(res => res.json())
       .then(parsedRes => {
         dispatch(uiStopLoading());
-        if (parsedRes.error) {
+
+        if (!parsedRes.idToken) {
           alert('Authentication failed, please try again!!');
         } else {
+          dispatch(authSetToken(parsedRes.idToken));
+
           startMainTabs();
         }
       });
+  };
+};
+
+export const authSetToken = token => {
+  return {
+    type: AUTH_SET_TOKEN,
+    token: token
   };
 };
