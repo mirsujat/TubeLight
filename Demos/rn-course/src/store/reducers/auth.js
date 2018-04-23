55ec3db8086436d568a50049eadb8d37f3b9abdd
mirsujat <mir.sujat@gmail.com>
2018-04-23 12:17:44 +0600

    mirsujat

---
+++ Demos/rn-course/src/store/reducers/auth.js
@@ -0,0 +1,16 @@
+import { AUTH_SET_TOKEN } from '../actions/actionTypes';
+
+const initialState = {
+  token: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case AUTH_SET_TOKEN:
+      return { ...state, token: action.token };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
