8724f38253bd5c30b0fa19122f762842f7899b70
mirsujat <mir.sujat@gmail.com>
2018-04-23 12:16:43 +0600

    mirsujat

--- Demos/rn-course/src/store/actions/actionTypes.js
+++ Demos/rn-course/src/store/actions/actionTypes.js
@@ -5,3 +5,5 @@ export const TRY_AUTH = 'TRY_AUTH';
 
 export const UI_START_LOADING = 'UI_START_LOADING';
 export const UI_STOP_LOADING = 'UI_STOP_LOADING';
+
+export const AUTH_SET_TOKEN = 'AUTH_SET_TOKEN';
