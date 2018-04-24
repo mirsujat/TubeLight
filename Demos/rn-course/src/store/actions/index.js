cb75ff48a2a688a19b91d53075a5c4fdda27ada3
mirsujat <mir.sujat@gmail.com>
2018-04-24 12:05:19 +0600

    user logout implemented

--- Demos/rn-course/src/store/actions/index.js
+++ Demos/rn-course/src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addPlace, deletePlace, getPlaces } from './places';
-export { tryAuth, authGetToken, authAutoSignIn } from './auth';
+export { tryAuth, authGetToken, authAutoSignIn, authLogout } from './auth';
 export { uiStartLoading, uiStopLoading } from './ui';
