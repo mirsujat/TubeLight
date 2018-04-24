a9207db6674b0558ce9ffe181556d39bce368a91
mirsujat <mir.sujat@gmail.com>
2018-04-24 14:35:41 +0600

    Refresh Auth Token

--- Demos/rn-course/src/store/actions/auth.js
+++ Demos/rn-course/src/store/actions/auth.js
@@ -58,9 +58,9 @@ export const tryAuth = (authData, authMode) => {
 
 export const authStoreToken = (token, expiresIn, refreshToken) => {
   return dispatch => {
-    dispatch(authSetToken(token));
     const now = new Date();
     const expiryDate = now.getTime() + expiresIn * 1000;
+    dispatch(authSetToken(token, expiryDate));
 
     AsyncStorage.setItem('ap:auth:token', token);
     AsyncStorage.setItem('ap:auth:expiryDate', expiryDate.toString());
@@ -68,10 +68,11 @@ export const authStoreToken = (token, expiresIn, refreshToken) => {
   };
 };
 
-export const authSetToken = token => {
+export const authSetToken = (token, expiryDate) => {
   return {
     type: AUTH_SET_TOKEN,
-    token: token
+    token: token,
+    expiryDate: expiryDate
   };
 };
 
@@ -79,8 +80,9 @@ export const authGetToken = () => {
   return (dispatch, getState) => {
     const promise = new Promise((resolve, reject) => {
       const token = getState().auth.token;
+      const expiryDate = getState.auth.expiryDate;
 
-      if (!token) {
+      if (!token || new Date(expiryDate) <= new Date()) {
         let fetchedToken;
         AsyncStorage.getItem('ap:auth:token')
           .catch(error => reject())
--- Demos/rn-course/src/store/reducers/auth.js
+++ Demos/rn-course/src/store/reducers/auth.js
@@ -1,15 +1,16 @@
 import { AUTH_SET_TOKEN, AUTH_REMOVE_TOKEN } from '../actions/actionTypes';
 
 const initialState = {
-  token: null
+  token: null,
+  expiryDate: null
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case AUTH_SET_TOKEN:
-      return { ...state, token: action.token };
+      return { ...state, token: action.token, expiryDate: action.expiryDate };
     case AUTH_REMOVE_TOKEN:
-      return { ...state, token: null };
+      return { ...state, token: null, expiryDate: null };
     default:
       return state;
   }
