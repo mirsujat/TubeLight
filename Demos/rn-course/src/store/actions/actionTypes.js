30bbb5a61aa81248ca5a0fec48da0c58b8cee493
mirsujat <mir.sujat@gmail.com>
2018-04-24 12:04:47 +0600

    user logout implemented

--- Demos/rn-course/src/store/actions/actionTypes.js
+++ Demos/rn-course/src/store/actions/actionTypes.js
@@ -7,3 +7,4 @@ export const UI_START_LOADING = 'UI_START_LOADING';
 export const UI_STOP_LOADING = 'UI_STOP_LOADING';
 
 export const AUTH_SET_TOKEN = 'AUTH_SET_TOKEN';
+export const AUTH_REMOVE_TOKEN = 'AUTH_REMOVE_TOKEN';
