ec7fb01a12d8c13822f562d6afdb92eba498ab24
mirsujat <mir.sujat@gmail.com>
2018-04-24 12:05:36 +0600

    user logout implemented

--- Demos/rn-course/src/store/reducers/auth.js
+++ Demos/rn-course/src/store/reducers/auth.js
@@ -1,4 +1,4 @@
-import { AUTH_SET_TOKEN } from '../actions/actionTypes';
+import { AUTH_SET_TOKEN, AUTH_REMOVE_TOKEN } from '../actions/actionTypes';
 
 const initialState = {
   token: null
@@ -8,6 +8,8 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case AUTH_SET_TOKEN:
       return { ...state, token: action.token };
+    case AUTH_REMOVE_TOKEN:
+      return { ...state, token: null };
     default:
       return state;
   }
