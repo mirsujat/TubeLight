2837751de63c1bb9aaa80ee4b730abce30474bd3
mirsujat <mir.sujat@gmail.com>
2018-04-25 16:59:37 +0600

    programatic navigation implemented

--- Demos/rn-course/src/store/actions/actionTypes.js
+++ Demos/rn-course/src/store/actions/actionTypes.js
@@ -1,5 +1,7 @@
 export const REMOVE_PLACE = 'REMOVE_PLACE';
 export const SET_PLACES = 'SET_PLACES';
+export const START_ADD_PLACE = 'START_ADD_PLACE';
+export const PLACE_ADDED = 'PLACE_ADDED';
 
 export const TRY_AUTH = 'TRY_AUTH';
 
