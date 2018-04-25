56deb89d89ff26e5dfd3dd4dd24225d2661444ba
mirsujat <mir.sujat@gmail.com>
2018-04-25 15:12:13 +0600

    Shringk Image Sizes

--- Demos/rn-course/src/components/PickImage/PickImage.js
+++ Demos/rn-course/src/components/PickImage/PickImage.js
@@ -8,18 +8,21 @@ class PickImage extends Component {
   };
 
   pickImageHandler = () => {
-    ImagePicker.showImagePicker({ title: 'Pick an Image' }, res => {
-      if (res.didCancel) {
-        console.log('User cancelled!');
-      } else if (res.error) {
-        console.log('Error', res.error);
-      } else {
-        this.setState({
-          pickedImage: { uri: res.uri }
-        });
-        this.props.onImagePicked({ uri: res.uri, base64: res.data });
+    ImagePicker.showImagePicker(
+      { title: 'Pick an Image', maxWidth: 800, maxHeight: 600 },
+      res => {
+        if (res.didCancel) {
+          console.log('User cancelled!');
+        } else if (res.error) {
+          console.log('Error', res.error);
+        } else {
+          this.setState({
+            pickedImage: { uri: res.uri }
+          });
+          this.props.onImagePicked({ uri: res.uri, base64: res.data });
+        }
       }
-    });
+    );
   };
 
   render() {
