0b008056a48213784d756adcfbd3c9b42a535305
mirsujat <mir.sujat@gmail.com>
2018-04-26 10:06:51 +0600

    Quick Bug Fix

--- Demos/rn-course/src/screens/FindPlace/FindPlace.js
+++ Demos/rn-course/src/screens/FindPlace/FindPlace.js
@@ -27,11 +27,12 @@ class FindPlaceScreen extends Component {
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
   }
 
-  componentDidMount() {
-    this.props.onLoadPlaces();
-  }
-
   onNavigatorEvent = event => {
+    if (event.type === 'ScreenChangedEvent') {
+      if (event.id === 'willAppear') {
+        this.props.onLoadPlaces();
+      }
+    }
     if (event.type === 'NavBarButtonPress') {
       if (event.id === 'sideDrawerToggle') {
         this.props.navigator.toggleDrawer({
