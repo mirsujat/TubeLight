e57dc2bbebb9685da8cb8eb418ac0af1e55007b7
mirsujat <mir.sujat@gmail.com>
2018-05-04 20:32:20 +0600

    accessing child props

--- react-16/src/App.js
+++ react-16/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi! i am a React App</h1>
+        <Person name="Mir" age="30" />
+        <Person name="Gyle" age="36">
+          My Hobbies: Travelling
+        </Person>
+        <Person name="Martin" age="26" />
       </div>
     );
   }
---
+++ react-16/src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'm a {props.name} and I am {props.age} Years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export default person;
