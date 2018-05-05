6245a382fe78926abc61b2fac9b7ed8acdb96255
mirsujat <mir.sujat@gmail.com>
2018-05-05 14:25:57 +0600

    Manupulate state Obj

--- react-16/src/App.js
+++ react-16/src/App.js
@@ -3,15 +3,42 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Mir', age: 28 },
+      { name: 'Gyle', age: 29 },
+      { name: 'Martin', age: 32 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Mir Sujat', age: 36 },
+        { name: 'Gyle', age: 29 },
+        { name: 'Martin', age: 24 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi! i am a React App</h1>
-        <Person name="Mir" age="30" />
-        <Person name="Gyle" age="36">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}>
           My Hobbies: Travelling
         </Person>
-        <Person name="Martin" age="26" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
