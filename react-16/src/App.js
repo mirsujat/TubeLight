afc45efd363f793f13b3a8eeadf522195043a3fb
mirsujat <mir.sujat@gmail.com>
2018-05-05 20:55:06 +0600

    Handleing conditional Rendering

--- react-16/src/App.js
+++ react-16/src/App.js
@@ -34,9 +34,9 @@ class App extends Component {
   };
 
   togglePersonHandler = () => {
-   const doesShow = this.state.showPersons;
-   this.setState({ showPersons: !doesShow })
-  }
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
     const style = {
@@ -45,33 +45,38 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Mir!')}
+            changed={this.nameChangedHandler}>
+            My Hobbies: Travelling
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
     }
 
     return (
       <div className="App">
         <h1>Hi! i am a React App</h1>
-        <button  style={style} onClick={this.togglePersonHandler}>
-          Togggle Persons 
+        <button style={style} onClick={this.togglePersonHandler}>
+          Togggle Persons
         </button>
-        { this.state.showPersons ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Mir!')}
-              changed={this.nameChangedHandler}>
-              My Hobbies: Travelling
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-        </div> : null
-        }
+        {persons}
       </div>
     );
   }
