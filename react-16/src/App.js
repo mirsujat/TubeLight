637b942ab59a283260826d8b8b0de48ada24199c
mirsujat <mir.sujat@gmail.com>
2018-05-06 15:16:19 +0600

    Settings className Dynamically

--- react-16/src/App.js
+++ react-16/src/App.js
@@ -82,9 +82,18 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); //classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); //classes = ['red', 'bold']
+    }
+
     return (
       <div className="App">
         <h1>Hi! i am a React App</h1>
+        <p className={classes.join(' ')}>This is really Working!!</p>
         <button style={style} onClick={this.togglePersonHandler}>
           Togggle Persons
         </button>
