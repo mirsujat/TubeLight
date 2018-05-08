2883cf7a4db54584c93830bf7b94746403855618
mirsujat <mir.sujat@gmail.com>
2018-05-08 10:27:29 +0600

    propTypes validation

--- react-16/src/components/Persons/Person/Person.js
+++ react-16/src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Style from './Person.css';
 
 const person = props => {
@@ -14,3 +16,33 @@ const person = props => {
 };
 
 export default person;
+
+// PropTypes only used in component class; here is an example of propTypes setup
+// PropTypes doesn't work in functional component
+
+// class Person extends Component {
+//   render() {
+//     return (
+//       <div className={Style.Person}>
+//         <p onClick={this.props.click}>
+//           I'm a {this.props.name} and I am {this.props.age} Years old!
+//         </p>
+//         <p>{this.props.children}</p>
+//         <input
+//           type="text"
+//           onChange={this.props.changed}
+//           value={this.props.name}
+//         />
+//       </div>
+//     );
+//   }
+// }
+//
+// Person.propTypes = {
+//   click: PropTypes.func,
+//   name: PropTypes.string,
+//   age: PropTypes.number,
+//   changed: PropTypes.func
+// }
+//
+// export default Person;
