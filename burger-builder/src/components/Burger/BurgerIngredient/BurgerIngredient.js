7c14fdbf57e947c28b899f4327752b7a4f122b88
Mir Sujat <mir.sujat@gmail.com>
2018-05-09 15:13:02 +0600

    BurgerIngredient component added

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -2,6 +2,43 @@ import React from 'react';
 
 import Style from './burgerIngredient.css';
 
-const burgerIngredient = props => {};
+const burgerIngredient = props => {
+	let ingredient = null;
+
+	switch (props.type) {
+		case 'bread-bottom':
+			ingredient = <div className={Style.BreadBottom}>todo</div>;
+			break;
+
+		case 'bread-top':
+			ingredient = (
+				<div className={Style.BreadTop}>
+					<div className={Style.Seeds1}>todo</div>
+					<div className={Style.Seeds2}>todo</div>
+				</div>
+			);
+			break;
+
+		case 'meat':
+			ingredient = <div className={Style.Meat}>todo</div>;
+			break;
+
+		case 'cheese':
+			ingredient = <div className={Style.Cheese}>todo</div>;
+			break;
+
+		case 'bacon':
+			ingredient = <div className={Style.Bacon}>todo</div>;
+			break;
+
+		case 'salad':
+			ingredient = <div className={Style.Salad}>todo</div>;
+			break;
+		default:
+			ingredient = null;
+	}
+
+	return ingredient;
+};
 
 export default burgerIngredient;
