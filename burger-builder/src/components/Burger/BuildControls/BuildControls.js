fe06566c2c7e6a1cd87c63e4539c51cae23f6ec0
Mir Sujat <mir.sujat@gmail.com>
2018-05-09 21:07:56 +0600

    Added Multiple BuildCOntrols

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const buildControls = props => <div>todo</div>;
+import Style from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+	{ label: 'Salad', type: 'salad' },
+	{ label: 'Cheese', type: 'cheese' },
+	{ label: 'Bacon', type: 'bacon' },
+	{ label: 'Meat', type: 'meat' }
+];
+
+const buildControls = props => (
+	<div className={Style.BuildControls}>
+		{controls.map(ctrl => <BuildControl key={ctrl.label} label={ctrl.label} />)}
+	</div>
+);
 
 export default buildControls;
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Wrapper from '../../hoc/Wrapper';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 	// constructor(props) {
@@ -21,7 +22,7 @@ class BurgerBuilder extends Component {
 		return (
 			<Wrapper>
 				<Burger ingredients={this.state.ingredients} />
-				<div>Build Controls</div>
+				<BuildControls />
 			</Wrapper>
 		);
 	}
