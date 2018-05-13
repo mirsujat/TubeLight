a39ded2df6ec62c50f71895da43a3352f80c5c7c
Mir Sujat <mir.sujat@gmail.com>
2018-05-13 20:34:51 +0600

    user can get single post

--- blog/src/components/FullPost/FullPost.js
+++ blog/src/components/FullPost/FullPost.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+	state = {
+		loadedPost: null
+	};
+	componentDidUpdate() {
+		if (this.props.id) {
+			if (
+				!this.state.loadedPost ||
+				(this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+			) {
+				axios
+					.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+					.then(response => {
+						this.setState({ loadedPost: response.data });
+					});
+			}
+		}
+	}
 
-        );
-        return post;
-    }
+	render() {
+		let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+		if (this.props.id) {
+			post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
+		}
+		if (this.state.loadedPost) {
+			post = (
+				<div className="FullPost">
+					<h1>{this.state.loadedPost.title}</h1>
+					<p>{this.state.loadedPost.content}</p>
+					<div className="Edit">
+						<button className="Delete">Delete</button>
+					</div>
+				</div>
+			);
+		}
+		return post;
+	}
 }
 
 export default FullPost;
