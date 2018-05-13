af6d0c37d20f406102d6798de0afa74e68dbfaef
Mir Sujat <mir.sujat@gmail.com>
2018-05-13 11:20:30 +0600

    improve performance and add Lifecycle Hook in Modal

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Style from './Modal.css';
 import Wrapper from '../../../hoc/Wrapper';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-	<Wrapper>
-		<Backdrop show={props.show} clicked={props.modalClosed} />
-		<div
-			className={Style.Modal}
-			style={{
-				transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-				opacity: props.show ? '1' : '0'
-			}}
-		>
-			{props.children}
-		</div>
-	</Wrapper>
-);
+class Modal extends Component {
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
 
-export default modal;
+	render() {
+		return (
+			<Wrapper>
+				<Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+				<div
+					className={Style.Modal}
+					style={{
+						transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+						opacity: this.props.show ? '1' : '0'
+					}}
+				>
+					{this.props.children}
+				</div>
+			</Wrapper>
+		);
+	}
+}
+
+export default Modal;
