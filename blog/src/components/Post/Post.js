c2f5f880b4519525e0de2bb8bf0fabfdfe379379
Mir Sujat <mir.sujat@gmail.com>
2018-05-13 19:43:44 +0600

    Fetch posts from server

--- blog/src/components/Post/Post.js
+++ blog/src/components/Post/Post.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
-        </div>
-    </article>
+const post = props => (
+	<article className="Post">
+		<h1>{props.title}</h1>
+		<div className="Info">
+			<div className="Author">{props.author}</div>
+		</div>
+	</article>
 );
 
 export default post;
--- blog/src/containers/Blog/Blog.js
+++ blog/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,40 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+	state = {
+		posts: []
+	};
+
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+			const posts = response.data.slice(0, 4);
+			const updatedPosts = posts.map(post => {
+				return {
+					...post,
+					author: 'Mir'
+				};
+			});
+			this.setState({ posts: updatedPosts });
+		});
+	}
+
+	render() {
+		const posts = this.state.posts.map(post => {
+			return <Post key={post.id} title={post.title} author={post.author} />;
+		});
+
+		return (
+			<div>
+				<section className="Posts">{posts}</section>
+				<section>
+					<FullPost />
+				</section>
+				<section>
+					<NewPost />
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Blog;
