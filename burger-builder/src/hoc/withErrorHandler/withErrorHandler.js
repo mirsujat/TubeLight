dee9211c213e8a95cff1a4087697f3a93a03b387
Mir Sujat <mir.sujat@gmail.com>
2018-05-14 16:59:54 +0600

    withErrorhandler component added

---
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import Wrapper from '../Wrapper/Wrapper';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+	return class extends Component {
+		state = {
+			error: null
+		};
+
+		componentDidMount() {
+			axios.interceptors.request.use(req => {
+				this.setState({ error: null });
+				return req;
+			});
+			axios.interceptors.response.use(
+				res => res,
+				error => {
+					this.setState({ error: error });
+				}
+			);
+		}
+
+		errorConfirmHandler = () => {
+			this.setState({ error: null });
+		};
+
+		render() {
+			return (
+				<Wrapper>
+					<Modal show={this.state.error} modalClosed={this.errorConfirmHandler}>
+						{this.state.error ? this.state.error.message : null}
+					</Modal>
+					<WrappedComponent {...this.props} />
+				</Wrapper>
+			);
+		}
+	};
+};
+
+export default withErrorHandler;
