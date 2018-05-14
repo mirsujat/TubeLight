2bce7e734f3adabbbe12ed3bbe2fac75812c2412
Mir Sujat <mir.sujat@gmail.com>
2018-05-14 11:46:41 +0600

    add interceptor

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(
+	request => {
+		console.log(request);
+		return request;
+	},
+	error => {
+		console.log(error);
+		return Promise.reject(error);
+	}
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
