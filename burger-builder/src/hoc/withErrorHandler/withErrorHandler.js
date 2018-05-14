cc5fb04edc70640da1787c95bfdba2a0b0bd308e
Mir Sujat <mir.sujat@gmail.com>
2018-05-14 20:06:55 +0600

    Removed old Interceptor  and prevent memory leaks

--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,16 +10,21 @@ const withErrorHandler = (WrappedComponent, axios) => {
 		};
 
 		componentWillMount() {
-			axios.interceptors.request.use(req => {
+			this.reqInterceptor = axios.interceptors.request.use(req => {
 				this.setState({ error: null });
 				return req;
 			});
-			axios.interceptors.response.use(
+			this.resInterceptor = axios.interceptors.response.use(
 				res => res,
 				error => {
 					this.setState({ error: error });
 				}
 			);
+		}
+
+		componentWillUnmount() {
+			axios.interceptors.request.eject(this.reqInterceptor);
+			axios.interceptors.response.eject(this.resInterceptor);
 		}
 
 		errorConfirmHandler = () => {
