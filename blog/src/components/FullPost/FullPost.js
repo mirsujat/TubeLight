54ef43b1262521719b56c95cc8ee526dff9b0b6f
Mir Sujat <mir.sujat@gmail.com>
2018-05-14 10:26:19 +0600

    Bug fix

--- blog/src/components/FullPost/FullPost.js
+++ blog/src/components/FullPost/FullPost.js
@@ -39,7 +39,7 @@ class FullPost extends Component {
 			post = (
 				<div className="FullPost">
 					<h1>{this.state.loadedPost.title}</h1>
-					<p>{this.state.loadedPost.content}</p>
+					<p>{this.state.loadedPost.body}</p>
 					<div className="Edit">
 						<button onClick={this.deletePostHandler} className="Delete">
 							Delete
