8f629a0bc90e00883e65d452fac65443810ba760
Mir Sujat <mir.sujat@gmail.com>
2018-05-16 10:23:10 +0600

    Conditional Redirect

--- blog/src/containers/Blog/NewPost/NewPost.js
+++ blog/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
 	state = {
 		title: '',
 		content: '',
-		author: 'Max'
+		author: 'Max',
+		submitted: false
 	};
 
 	componentDidMount() {
@@ -27,12 +29,22 @@ class NewPost extends Component {
 		};
 		axios.post('/posts/', data).then(response => {
 			console.log(response);
+			this.setState({ submitted: true });
 		});
 	};
 
 	render() {
+		{
+			/* Conditional Redirect*/
+		}
+		let redirect = null;
+		if (this.state.submitted) {
+			redirect = <Redirect to="/posts" />;
+		}
+
 		return (
 			<div className="NewPost">
+				{redirect}
 				<h1>Add a Post</h1>
 				<label>Title</label>
 				<input
