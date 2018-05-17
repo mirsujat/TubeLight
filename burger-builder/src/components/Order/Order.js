30c89a722e52e63b7df6caaaf02f5ad510ff2cbe
Mir Sujat <mir.sujat@gmail.com>
2018-05-17 16:45:15 +0600

    output the order

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -2,13 +2,39 @@ import React from 'react';
 
 import Style from './Order.css';
 
-const order = props => (
-	<div className={Style.Order}>
-		<p>Ingredients: Salad (1)</p>
-		<p>
-			Price: <strong>USD 5.45</strong>
-		</p>
-	</div>
-);
+const order = props => {
+	const ingredients = [];
+	for (let ingredientName in props.ingredients) {
+		ingredients.push({
+			name: ingredientName,
+			amount: props.ingredients[ingredientName]
+		});
+	}
+
+	const ingredientOutput = ingredients.map(ig => {
+		return (
+			<span
+				key={ig.name}
+				style={{
+					textTransform: 'capitalize',
+					display: 'inline-block',
+					margin: '0 8px',
+					border: '1px solid #ccc',
+					padding: '5px'
+				}}>
+				{ig.name} ({ig.amount})
+			</span>
+		);
+	});
+
+	return (
+		<div className={Style.Order}>
+			<p>Ingredients: {ingredientOutput}</p>
+			<p>
+				Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+			</p>
+		</div>
+	);
+};
 
 export default order;
--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -33,8 +33,13 @@ class Orders extends Component {
 	render() {
 		return (
 			<div>
-				<Order />
-				<Order />
+				{this.state.orders.map(order => (
+					<Order
+						key={order.id}
+						ingredients={order.ingredients}
+						price={order.price}
+					/>
+				))}
 			</div>
 		);
 	}
