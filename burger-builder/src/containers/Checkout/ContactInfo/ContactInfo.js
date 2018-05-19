fbdf420fc8d5947bbf7ba0a78ff6f505bd62ec84
Mir Sujat <mir.sujat@gmail.com>
2018-05-19 12:48:34 +0600

    Bug fix

--- burger-builder/src/containers/Checkout/ContactInfo/ContactInfo.js
+++ burger-builder/src/containers/Checkout/ContactInfo/ContactInfo.js
@@ -85,6 +85,7 @@ class ContactInfo extends Component {
 					]
 				},
 				value: '',
+				validation: {},
 				valid: true
 			}
 		},
@@ -119,6 +120,10 @@ class ContactInfo extends Component {
 
 	checkValidity(value, rules) {
 		let isValid = true;
+
+		if (!rules) {
+			return true;
+		}
 
 		if (rules.required) {
 			isValid = value.trim() !== '' && isValid;
