544b9e7a7007f0b4c5a26c99ca8b0118ebcfe109
Mir Sujat <mir.sujat@gmail.com>
2018-05-19 17:42:20 +0600

    mirsujat

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
@@ -9,5 +10,10 @@ import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
--- redux-counter/src/store/reducer.js
+++ redux-counter/src/store/reducer.js
@@ -3,6 +3,11 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+	if (action.type === 'INCREMENT') {
+		return {
+			counter: state.counter + 1
+		};
+	}
 	return state;
 };
 
