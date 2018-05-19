5cff2e1748699499275db48bb27eaacec8772ef7
Mir Sujat <mir.sujat@gmail.com>
2018-05-19 17:08:53 +0600

    mirsujat

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
