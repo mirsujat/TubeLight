6242896f0f639fc2d5ff06ab8b8eec907ecd7c8c
Mir Sujat <mir.sujat@gmail.com>
2018-05-23 14:32:31 +0600

    create action creator for burgerBuilder

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/actions';
+import * as action from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
 	state = {
@@ -138,10 +138,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onIngredientAdded: ingName =>
-			dispatch({ type: actionTypes.ADD_INGREDIENT, ingredientName: ingName }),
-		onIngredientRemoved: ingName =>
-			dispatch({ type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName })
+		onIngredientAdded: ingName => dispatch(action.addIngredient(ingName)),
+		onIngredientRemoved: ingName => dispatch(action.removeIngredient(ingName))
 	};
 };
 
--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -7,10 +7,10 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
 const store = createStore(
-	reducer,
+	burgerBuilderReducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
