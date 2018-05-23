438a82bc61ff2997f4e31debf1f53930eb5ca42a
Mir Sujat <mir.sujat@gmail.com>
2018-05-23 11:23:40 +0600

    use utility function to clean reducer

--- redux-counter/src/store/reducers/counter.js
+++ redux-counter/src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
 	counter: 0
@@ -7,27 +8,16 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.INCREMENT:
-			const newState = Object.assign({}, state);
-			newState.counter = state.counter + 1;
-			return newState;
+			return updateObject(state, { counter: state.counter + 1 });
 
 		case actionTypes.DECREMENT:
-			return {
-				...state,
-				counter: state.counter - 1
-			};
+			return updateObject(state, { counter: state.counter - 1 });
 
 		case actionTypes.ADD:
-			return {
-				...state,
-				counter: state.counter + action.val
-			};
+			return updateObject(state, { counter: state.counter + action.val });
 
 		case actionTypes.SUBTRACT:
-			return {
-				...state,
-				counter: state.counter - action.val
-			};
+			return updateObject(state, { counter: state.counter - action.val });
 
 		default:
 			return state;
--- redux-counter/src/store/reducers/result.js
+++ redux-counter/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
 	results: []
@@ -7,23 +8,15 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.STORE_RESULT:
-			return {
-				...state,
+			return updateObject(state, {
 				results: state.results.concat({ id: new Date(), value: action.result })
-			};
+			});
 
 		case actionTypes.DELETE_RESULT:
-			// const id = 2;
-			// const newArray = [...state.results];
-			// newArray.splice(id, 1);
-
 			const updatedArray = state.results.filter(
 				result => result.id !== action.resultElementId
 			);
-			return {
-				...state,
-				results: updatedArray
-			};
+			return updateObject(state, { results: updatedArray });
 
 		default:
 			return state;
