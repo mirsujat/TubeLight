db73c711d682e9f61903bb31a5847e339d8013db
Mir Sujat <mir.sujat@gmail.com>
2018-05-23 17:58:12 +0600

    Redirect to Improve UX

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -36,19 +36,23 @@ class Checkout extends Component {
 	};
 
 	render() {
-		return (
-			<div>
-				<CheckoutSummary
-					checkoutCancelled={this.checkoutCancelledHandler}
-					checkoutContinued={this.checkoutContinuedHandler}
-					ingredients={this.props.ings}
-				/>
-				<Route
-					path={this.props.match.path + '/contact-info'}
-					component={ContactInfo}
-				/>
-			</div>
-		);
+		let summary = <Redirect to="/" />;
+		if (this.props.ings) {
+			summary = (
+				<div>
+					<CheckoutSummary
+						checkoutCancelled={this.checkoutCancelledHandler}
+						checkoutContinued={this.checkoutContinuedHandler}
+						ingredients={this.props.ings}
+					/>
+					<Route
+						path={this.props.match.path + '/contact-info'}
+						component={ContactInfo}
+					/>
+				</div>
+			);
+		}
+		return summary;
 	}
 }
 
