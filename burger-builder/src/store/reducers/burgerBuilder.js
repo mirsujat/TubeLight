e3f7a0be84a3ec6ffb200bc006a71ce472c7c3eb
Mir Sujat <mir.sujat@gmail.com>
2018-05-24 14:03:37 +0600

    Refactor Reducers using utility function

--- burger-builder/src/store/reducers/burgerBuilder.js
+++ burger-builder/src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
 	ingredients: null,
@@ -16,14 +17,19 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.ADD_INGREDIENT:
-			return {
-				...state,
-				ingredients: {
-					...state.ingredients,
-					[action.ingredientName]: state.ingredients[action.ingredientName] + 1
-				},
+			const updatedIngredient = {
+				[action.ingredientName]: state.ingredients[action.ingredientName] + 1
+			};
+			const updatedIngredients = updateObject(
+				state.ingredients,
+				updatedIngredient
+			);
+			const updatedState = {
+				ingredients: updatedIngredients,
 				totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
 			};
+			return updateObject(state, updatedState);
+
 		case actionTypes.REMOVE_INGREDIENT:
 			return {
 				...state,
@@ -35,18 +41,15 @@ const reducer = (state = initialState, action) => {
 			};
 
 		case actionTypes.SET_INGREDIENTS:
-			return {
-				...state,
+			return updateObject(state, {
 				ingredients: action.ingredients,
 				totalPrice: 4,
 				error: false
-			};
+			});
 
 		case actionTypes.FETCH_INGREDIENTS_FAILED:
-			return {
-				...state,
-				error: true
-			};
+			return updateObject(state, { error: true });
+
 		default:
 			return state;
 	}
