680c091d9486f00f3f472568adc321d69ff194c9
Mir Sujat <mir.sujat@gmail.com>
2018-05-24 17:06:33 +0600

    InputChangedHandler Implemented

--- burger-builder/src/containers/Auth/Auth.js
+++ burger-builder/src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Style from './Auth.css';
 
 class Auth extends Component {
 	state = {
@@ -37,6 +38,43 @@ class Auth extends Component {
 		}
 	};
 
+	checkValidity(value, rules) {
+		let isValid = true;
+
+		if (!rules) {
+			return true;
+		}
+
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength && isValid;
+		}
+		return isValid;
+	}
+
+	inputChangedHandler = (event, controlName) => {
+		const updatedControls = {
+			...this.state.controls,
+			[controlName]: {
+				...this.state.controls[controlName],
+				value: event.target.value,
+				valid: this.checkValidity(
+					event.target.value,
+					this.state.controls[controlName].validation
+				),
+				touched: true
+			}
+		};
+
+		this.setState({ controls: updatedControls });
+	};
+
 	render() {
 		const formElementsArray = [];
 		for (let key in this.state.controls) {
@@ -62,7 +100,7 @@ class Auth extends Component {
 		));
 
 		return (
-			<div>
+			<div className={Style.Auth}>
 				<form>
 					{form}
 					<Button btnType="Success">SUBMIT</Button>
