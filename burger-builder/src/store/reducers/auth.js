d983ca305f6b6452b7535906d6e3ebcfd04adb63
Mir Sujat <mir.sujat@gmail.com>
2018-05-24 20:25:40 +0600

    Store auth token

---
+++ burger-builder/src/store/reducers/auth.js
@@ -0,0 +1,44 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
+const initialState = {
+	token: null,
+	userId: null,
+	error: null,
+	loading: false
+};
+
+const authStart = (state, action) => {
+	return updateObject(state, { error: null, loading: true });
+};
+
+const authSuccess = (state, action) => {
+	return updateObject(state, {
+		token: action.idToken,
+		userId: action.userId,
+		error: null,
+		loading: false
+	});
+};
+
+const authFailed = (state, action) => {
+	return updateObject(state, { error: action.error, loading: false });
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.AUTH_START:
+			return authStart(state, action);
+
+		case actionTypes.AUTH_SUCCESS:
+			return authSuccess(state, action);
+
+		case actionTypes.AUTH_FAILED:
+			return authFailed(state, action);
+
+		default:
+			return state;
+	}
+};
+
+export default reducer;
