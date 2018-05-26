30c8a5e775e05bda5082250ce6d33023961297df
Mir Sujat <mir.sujat@gmail.com>
2018-05-26 15:38:35 +0600

    Adding Lazy Loading

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as action from './store/actions/index';
+
+// Lazy Loading
+const asyncCheckout = asyncComponent(() => {
+	return import('./containers/Checkout/Checkout');
+});
+
+// Lazy Loading
+const asyncOrders = asyncComponent(() => {
+	return import('./containers/Orders/Orders');
+});
+
+// Lazy Loading
+const asyncAuth = asyncComponent(() => {
+	return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
 	componentDidMount() {
@@ -18,7 +31,7 @@ class App extends Component {
 	render() {
 		let routes = (
 			<Switch>
-				<Route path="/auth" component={Auth} />
+				<Route path="/auth" component={asyncAuth} />
 				<Route path="/" exact component={BurgerBuilder} />
 				<Redirect to="/" />
 			</Switch>
@@ -26,10 +39,10 @@ class App extends Component {
 		if (this.props.isAuthenticated) {
 			routes = (
 				<Switch>
-					<Route path="/checkout" component={Checkout} />
-					<Route path="/orders" component={Orders} />
+					<Route path="/checkout" component={asyncCheckout} />
+					<Route path="/orders" component={asyncOrders} />
 					<Route path="/logout" component={Logout} />
-					<Route path="/auth" component={Auth} />
+					<Route path="/auth" component={asyncAuth} />
 					<Route path="/" exact component={BurgerBuilder} />
 					<Redirect to="/" />
 				</Switch>
