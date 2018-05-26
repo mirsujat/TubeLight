5b2a8fe58acec3062ee01d001e5cde188bd77a14
Mir Sujat <mir.sujat@gmail.com>
2018-05-26 10:50:38 +0600

    Persistant state with localStorage

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,8 +8,13 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as action from './store/actions/index';
 
 class App extends Component {
+	componentDidMount() {
+		this.props.tryAutoLogin();
+	}
+
 	render() {
 		return (
 			<div>
@@ -26,4 +32,10 @@ class App extends Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+	return {
+		tryAutoLogin: () => dispatch(action.authCheckState())
+	};
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
