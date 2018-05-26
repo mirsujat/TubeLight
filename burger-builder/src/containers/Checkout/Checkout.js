c2793f92c8971d9ea53b3530aa1af394f91664c1
Mir Sujat <mir.sujat@gmail.com>
2018-05-26 15:12:36 +0600

    Remove console.log() statement

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -4,30 +4,8 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactInfo from './ContactInfo/ContactInfo';
-//import * as action from '../../store/actions/index';
 
 class Checkout extends Component {
-	// With Redux. We can get Ingredients from redux store
-
-	// Without Redux. We can get Ingredients through query params and local state
-	// componentWillMount() {
-	// 	// The URLSearchParams interface defines utility methods to work with the query string of a URL.
-	// 	// An object implementing URLSearchParams can directly be used in a for...of structure, instead of entries(): for (var p of mySearchParams) is equivalent to for (var p of mySearchParams.entries())
-	//
-	// 	const query = new URLSearchParams(this.props.location.search);
-	// 	const ingredients = {};
-	// 	let price = 0;
-	// 	for (let param of query.entries()) {
-	// 		// ['salad', '1']
-	// 		if (param[0] === 'price') {
-	// 			price = param[1];
-	// 		} else {
-	// 			ingredients[param[0]] = +param[1];
-	// 		}
-	// 	}
-	// 	this.setState({ ingredients: ingredients, totalPrice: price });
-	// }
-
 	checkoutCancelledHandler = () => {
 		this.props.history.goBack();
 	};
