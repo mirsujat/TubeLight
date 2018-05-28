d18542835cc8066f7773f3df3824bd76d9daa13f
Mir Sujat <mir.sujat@gmail.com>
2018-05-28 15:53:11 +0600

    Basic style

---
+++ nextjs-starter/components/User.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const user = props => (
+	<div>
+		<h1>Name: {props.name}</h1>
+		<p>Age: {props.age}</p>
+	</div>
+);
+
+export default user;
--- nextjs-starter/pages/auth/index.js
+++ nextjs-starter/pages/auth/index.js
@@ -1,8 +1,27 @@
 import react from 'react';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
 	<div>
 		<h1>The Auth Index Page</h1>
+		<User name="Mir Sujat" age={36} />
+		<style jsx>{`
+			p {
+				color: blue;
+			}
+			div {
+				border: 1px solid #eee;
+				box-shadow: 0 2px 3px #ccc;
+				padding: 20px;
+				text-align: center;
+			}
+			@media (max-width: 600px) {
+				div {
+					background: blue;
+				}
+			}
+		`}</style>
 	</div>
 );
 
