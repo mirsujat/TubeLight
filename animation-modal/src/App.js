9466332a8afc1d5915881975332e0b998e93255d
Mir Sujat <mir.sujat@gmail.com>
2018-05-29 16:20:39 +0600

    Css Animation added

--- animation-modal/src/App.js
+++ animation-modal/src/App.js
@@ -1,35 +1,41 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./App.css";
-import Modal from "./components/Modal/Modal";
-import Backdrop from "./components/Backdrop/Backdrop";
-import List from "./components/List/List";
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
 
 class App extends Component {
-  state = {
-    modalIsOpen: false
-  }
+	state = {
+		modalIsOpen: false
+	};
 
-  showModal = () => {
-    this.setState({modalIsOpen: true});
-  }
+	showModal = () => {
+		this.setState({ modalIsOpen: true });
+	};
 
-  closeModal = () => {
-    this.setState({modalIsOpen: false});
-  }
+	closeModal = () => {
+		this.setState({ modalIsOpen: false });
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen} />
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>React Animations</h1>
+				{this.state.modalIsOpen ? (
+					<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+				) : null}
+				{this.state.modalIsOpen ? (
+					<Backdrop show={this.state.modalIsOpen} />
+				) : null}
+				<button className="Button" onClick={this.showModal}>
+					Open Modal
+				</button>
+				<h3>Animating Lists</h3>
+				<List />
+			</div>
+		);
+	}
 }
 
 export default App;
