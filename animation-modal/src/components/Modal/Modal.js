d24eb33904f07faf7ed8388a65b34e981cfd4878
Mir Sujat <mir.sujat@gmail.com>
2018-05-29 20:16:14 +0600

    diffrent timing added

--- animation-modal/src/components/Modal/Modal.js
+++ animation-modal/src/components/Modal/Modal.js
@@ -3,9 +3,14 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const TIME = {
+	enter: 400,
+	exit: 2000
+};
+
 const modal = props => {
 	return (
-		<Transition in={props.show} mountOnEnter unmountOnExit timeout={300}>
+		<Transition in={props.show} mountOnEnter unmountOnExit timeout={TIME}>
 			{state => {
 				const Style = [
 					'Modal',
