ccdefd5c953caeb7a47572939964a61c08f04671
Mir Sujat <mir.sujat@gmail.com>
2018-05-30 19:28:05 +0600

    authCheckStateSaga Implemented

--- redux-saga/src/store/actions/auth.js
+++ redux-saga/src/store/actions/auth.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,9 +22,6 @@ export const authFailed = error => {
 };
 
 export const logout = () => {
-	// localStorage.removeItem('token');
-	// localStorage.removeItem('expirationDate');
-	// localStorage.removeItem('userId');
 	return {
 		type: actionTypes.AUTH_INITIATE_LOGOUT
 	};
@@ -59,23 +55,7 @@ export const setAuthRedirectPath = path => {
 };
 
 export const authCheckState = () => {
-	return dispatch => {
-		const token = localStorage.getItem('token');
-		if (!token) {
-			dispatch(logout());
-		} else {
-			const expirationDate = new Date(localStorage.getItem('expirationDate'));
-			if (expirationDate <= new Date()) {
-				dispatch(logout());
-			} else {
-				const userId = localStorage.getItem('userId');
-				dispatch(authSuccess(token, userId));
-				dispatch(
-					checkAuthTimeout(
-						(expirationDate.getTime() - new Date().getTime()) / 1000
-					)
-				);
-			}
-		}
+	return {
+		type: actionTypes.AUTH_CHECK_STATE
 	};
 };
--- redux-saga/src/store/sagas/index.js
+++ redux-saga/src/store/sagas/index.js
@@ -1,10 +1,16 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga, checkAuthTimeoutSaga, authSaga } from './auth';
+import {
+	logoutSaga,
+	checkAuthTimeoutSaga,
+	authSaga,
+	authCheckStateSaga
+} from './auth';
 
 export function* watchAuth() {
 	yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
 	yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
 	yield takeEvery(actionTypes.AUTH_USER, authSaga);
+	yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
