6561ff4fda2f111173fb02bd60dac9af642d17ab
Mir Sujat <mir.sujat@gmail.com>
2018-05-30 12:56:36 +0600

    logoutSaga implemented

--- redux-saga/src/store/actions/auth.js
+++ redux-saga/src/store/actions/auth.js
@@ -23,11 +23,12 @@ export const authFailed = error => {
 };
 
 export const logout = () => {
-	localStorage.removeItem('token');
-	localStorage.removeItem('expirationDate');
-	localStorage.removeItem('userId');
+	// localStorage.removeItem('token');
+	// localStorage.removeItem('expirationDate');
+	// localStorage.removeItem('userId');
+
 	return {
-		type: actionTypes.AUTH_LOGOUT
+		type: actionTypes.AUTH_INITIATE_LOGOUT
 	};
 };
 
