7ed978093631bc8ee4d7c1d2e12b2c5aec117d05
Mir Sujat <mir.sujat@gmail.com>
2018-06-05 17:18:41 +0600

    mirsujat

---
+++ moringa/src/components/Card.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Row, Col, Card, CardTitle } from 'react-materialize';
+
+class Cards extends Component {
+	render() {
+		return (
+			<Row>
+				<Col m={6} s={12}>
+					<Card
+						header={<CardTitle reveal image="img/office.jpg" waves="light" />}
+						title="Card Title"
+						reveal={
+							<p>
+								Here is some more information about this product that is only
+								revealed once clicked on.
+							</p>
+						}>
+						<p>
+							<a href="">This is a link</a>
+						</p>
+					</Card>
+				</Col>
+			</Row>
+		);
+	}
+}
+
+export default Cards;
