bb4bf4f93737ef297bc4e46c554d7b9c07363c42
Mir Sujat <mir.sujat@gmail.com>
2018-06-06 15:37:39 +0600

    slick-carosousel added

--- web-store/src/App.js
+++ web-store/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { Row, Col } from 'react-materialize';
 
 import Card from './components/Card/Card';
