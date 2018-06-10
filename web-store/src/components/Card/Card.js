6e413979dfdc1a86677fc0637552e8b748459654
Mir Sujat <mir.sujat@gmail.com>
2018-06-10 15:27:16 +0600

    mirsujat

--- web-store/src/components/Card/Card.js
+++ web-store/src/components/Card/Card.js
@@ -6,18 +6,38 @@ class Card extends Component {
 	render() {
 		return (
 			<div className="col s3">
-				<div className="card">
+				<div className="card sticky-action">
 					<div className="card-image waves-effect waves-block waves-light">
 						<ImageSlider className="activator" />
 					</div>
 					<div className="card-content">
 						<span className="card-title activator grey-text text-darken-4">
-							Card Title<i className="material-icons right">more_vert</i>
+							<a href="">
+								fashion men lightweight action sports running shoes knitting
+								running shoes{' '}
+							</a>
+							<i className="material-icons right">more_vert</i>
 						</span>
+						<h5 className="Price">
+							US $ 8.50 <span className="Text-Normal">/ Pairs</span>{' '}
+						</h5>
+						<h5 className="Price">
+							One Pairs <span className="Text-Normal">(Min. Order)</span>
+						</h5>
 						<p>
-							<a href="">This is a link</a>
+							<a href="">Company: NiKi</a>
 						</p>
 					</div>
+
+					<div className="card-action">
+						<a className="waves-effect waves-light btn teal lighten-2">
+							SHOP NOW
+						</a>
+						<span className="right">
+							<i className="small material-icons">add_shopping_cart</i>
+						</span>
+					</div>
+
 					<div className="card-reveal">
 						<span className="card-title grey-text text-darken-4">
 							Card Title<i className="material-icons right">close</i>
