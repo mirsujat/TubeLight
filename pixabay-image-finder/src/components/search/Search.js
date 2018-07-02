54699ba3c2a4de57582031b091ca910d7cc7930a
Mir Sujat <mir.sujat@gmail.com>
2018-07-02 12:59:19 +0600

    mirsujat

--- pixabay-image-finder/src/components/search/Search.js
+++ pixabay-image-finder/src/components/search/Search.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
 
 class Search extends Component {
 	state = {
@@ -12,10 +13,20 @@ class Search extends Component {
 		images: []
 	};
 	onTextChange = e => {
-		this.setState({ [e.target.name]: e.target.value });
+		this.setState({ [e.target.name]: e.target.value }, () => {
+			axios
+				.get(
+					`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+						this.state.searchText
+					}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+				)
+				.then(res => this.setState({ images: res.data.hits }))
+				.catch(err => console.log(err));
+		});
 	};
 
 	render() {
+		console.log(this.state.images);
 		return (
 			<div>
 				<TextField
