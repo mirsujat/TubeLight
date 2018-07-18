219461f76c28152daa9d7a11fea77267a1b21bc4
Mir Sujat <mir.sujat@gmail.com>
2018-07-18 20:25:15 +0600

    mir

--- mdb-4/src/components/SearchForm.js
+++ mdb-4/src/components/SearchForm.js
@@ -10,7 +10,7 @@ class SearchForm extends Component{
         <div className="input-group md-form form-sm form-1 pl-0">
           <input className="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search" />
           <div className="input-group-append">
-            <span className="input-group-text grey lighten-3" id="basic-text1"><i className="fa fa-search  darken-4" aria-hidden="true"></i></span>
+            <span className="input-group-text" id="basic-text1"><i className="fa fa-search" aria-hidden="true"></i></span>
           </div>
         </div>
       </div>
