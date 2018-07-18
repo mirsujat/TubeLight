4f22fdb183e9484fe1a511d4b75b0dbf13e5ac3e
Mir Sujat <mir.sujat@gmail.com>
2018-07-18 20:27:37 +0600

    mirsujat

--- mdb-4/src/components/SearchForm.js
+++ mdb-4/src/components/SearchForm.js
@@ -5,8 +5,6 @@ class SearchForm extends Component{
   render() {
     return (
       <div className="container">
-        
-        
         <div className="input-group md-form form-sm form-1 pl-0">
           <input className="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search" />
           <div className="input-group-append">
