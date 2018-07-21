0a220be9191cd411d4d617e8e4b959f043853ce0
Mir Sujat <mir.sujat@gmail.com>
2018-07-21 21:33:16 +0600

    mir

--- mdb-nav/src/App.js
+++ mdb-nav/src/App.js
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './App.css';
 
+import NavbarMobile from './components/Navbar/Navbar_Mobile';
 import NavbarDesktop from './components/Navbar/Navbar_Desktop';
 import SearchForm from './components/SearchForm/SearchForm';
-import SearchFormSm from './components/SearchForm/SearchFormSm';
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <div className="mobile">
-        <SearchFormSm />
+        <NavbarMobile />
         </div>
         <div className="desktop">
         <NavbarDesktop />
