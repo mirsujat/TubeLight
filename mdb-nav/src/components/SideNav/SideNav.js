ad8d49e1366d1ed2579a1e91387fb3f6215152e6
Mir Sujat <mir.sujat@gmail.com>
2018-07-22 18:10:18 +0600

    mir

--- mdb-nav/src/components/SideNav/SideNav.js
+++ mdb-nav/src/components/SideNav/SideNav.js
@@ -4,11 +4,31 @@ import { ListGroup, ListGroupItem } from "mdbreact";
 const SideNav = props => {
   return (
     <ListGroup>
-      <ListGroupItem active>Home</ListGroupItem>
-      <ListGroupItem>Trending</ListGroupItem>
-      <ListGroupItem>Subscriptions</ListGroupItem>
-      <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-      <ListGroupItem>Vestibulum at eros</ListGroupItem>
+      <ListGroupItem active>
+        <span className="side-nav-icon active">
+          <i class="fa fa-home" aria-hidden="true" />
+        </span>Home
+      </ListGroupItem>
+      <ListGroupItem>
+        <span className="side-nav-icon">
+          <i class="fa fa-home" aria-hidden="true" />
+        </span>Trending
+      </ListGroupItem>
+      <ListGroupItem>
+        <span className="side-nav-icon">
+          <i class="fa fa-home" aria-hidden="true" />
+        </span>Subscriptions
+      </ListGroupItem>
+      <ListGroupItem>
+        <span className="side-nav-icon">
+          <i class="fa fa-home" aria-hidden="true" />
+        </span>Porta ac consectetur ac
+      </ListGroupItem>
+      <ListGroupItem>
+        <span className="side-nav-icon">
+          <i class="fa fa-home" aria-hidden="true" />
+        </span>Vestibulum at eros
+      </ListGroupItem>
     </ListGroup>
   );
 };
