42091c335400e764152f5197165ae51c7797b7b2
Mir Sujat <mir.sujat@gmail.com>
2018-07-29 10:54:44 +0600

    MUi Eshop init

---
+++ mui-eshop/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">MUI E-shop</div>;
+  }
+}
+
+export default App;
