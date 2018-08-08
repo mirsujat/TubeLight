469400890a98abd9acc79729f3f2b786ddbc6e14
Mir Sujat <mir.sujat@gmail.com>
2018-08-08 20:24:01 +0600

    NavbarNotification done

--- mui-custom-theme/src/App.jsx
+++ mui-custom-theme/src/App.jsx
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 
 import NavbarIcon from "./components/Navbar/NavbarIcon.jsx";
 import NavbarIconText from "./components/Navbar/NavbarIconText.jsx";
+import NavbarNotification from "./components/Navbar/NavbarNotification.jsx";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <h2>React Custom Theme</h2>
         <NavbarIcon />
         <NavbarIconText />
-        <h2>React Custom Theme</h2>
+        <NavbarNotification />
       </div>
     );
   }
