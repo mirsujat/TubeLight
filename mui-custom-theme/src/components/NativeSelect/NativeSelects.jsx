8787aee9f86bebf73b00fcadb4d68bc074b12f9f
Mir Sujat <mir.sujat@gmail.com>
2018-08-13 19:04:00 +0600

    mir

--- mui-custom-theme/src/components/NativeSelect/NativeSelects.jsx
+++ mui-custom-theme/src/components/NativeSelect/NativeSelects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import NativeSelect from "@material-ui/core/NativeSelect";
@@ -22,15 +23,23 @@ class NativeSelects extends React.Component {
     const { classes } = this.props;
 
     return (
-      <FormControl>
-        <InputLabel className={classes.inputLabel} htmlFor="age">
+      <FormControl className={classes.formControl}>
+        <InputLabel className={classes.inputLabel} shrink={false} htmlFor="age">
           Age
         </InputLabel>
 
         <NativeSelect
           value={this.state.age}
           onChange={this.handleChange("age")}
-          input={<Input name="age" id="age" />}
+          className={classes.select}
+          input={
+            <Input
+              className={classes.input}
+              disableUnderline={true}
+              name="age"
+              id="age"
+            />
+          }
         >
           <option value="" />
           <option value={10}>Ten</option>
--- mui-custom-theme/src/components/NativeSelect/NativeSelectsStyle.jsx
+++ mui-custom-theme/src/components/NativeSelect/NativeSelectsStyle.jsx
@@ -1,7 +1,46 @@
 const nativeSelectsStyle = {
+  formControl: {
+    display: "block",
+    position: "relative"
+  },
   inputLabel: {
-    display: "inline",
+    display: "inline-block",
+    position: "relative",
+    marginRight: "10px",
+    marginLeft: "10px",
+    backgroundColor: "yellow",
     color: "red"
+  },
+  input: {
+    padding: "6px 10px",
+    color: "#495057",
+    border: "1px solid #D2D2D2 !important",
+    "&,&::placeholder": {
+      fontSize: "0.93rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      fontWeight: "400",
+      lineHeight: "0.95rem",
+      opacity: "1"
+    },
+    "&::placeholder": {
+      color: "#AAAAAA"
+    },
+    "&:focus": {
+      border: " 1px solid #4ba6e4 !important",
+      backgroundColor: "white"
+    }
+  },
+  select: {
+    width: "100px",
+    cursor: "pointer",
+    minWidth: "50px",
+    userSelect: "none",
+    background: "white",
+    border: " 1px solid #4ba6e4 !important",
+    "&:focus": {
+      background: "white",
+      border: " 1px solid #4ba6e4 !important"
+    }
   }
 };
 
