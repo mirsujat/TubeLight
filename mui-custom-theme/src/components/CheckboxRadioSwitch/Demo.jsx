2e79024c805b6499be69c92c396966bd5e71f68f
Mir Sujat <mir.sujat@gmail.com>
2018-08-16 20:05:08 +0600

    mir

---
+++ mui-custom-theme/src/components/CheckboxRadioSwitch/Demo.jsx
@@ -0,0 +1,347 @@
+import React, { Component } from "react";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Radio from "@material-ui/core/Radio";
+import Switch from "@material-ui/core/Switch";
+
+// core components
+import GridContainer from "../Grid/GridContainer.jsx";
+import GridItem from "../Grid/GridItem.jsx";
+
+import demoStyle from "./demoStyle.jsx";
+
+class CheckboxRadioSwitchDemo extends Component {
+  state = {
+    checked: [24, 22],
+    selectedEnabled: "b",
+    checkedA: true,
+    checkedB: false
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+  handleChangeEnabled = event => {
+    this.setState({ selectedEnabled: event.target.value });
+  };
+  handleToggle = value => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.container}>
+        <div className={classes.title}>
+          <h3>
+            <small>Pagination for Lazy Loading</small>
+          </h3>
+        </div>
+        <div className={classes.space70} />
+        <div id="checkRadios">
+          <GridContainer>
+            <GridItem xs={12} sm={6} md={4} lg={3}>
+              <div className={classes.title}>
+                <h3>Checkboxes</h3>
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      tabIndex={-1}
+                      onClick={() => this.handleToggle(21)}
+                      checkedIcon={<Check className={classes.checkedIcon} />}
+                      icon={<Check className={classes.uncheckedIcon} />}
+                      classes={{ checked: classes.checked }}
+                    />
+                  }
+                  classes={{ label: classes.label }}
+                  label="Unchecked"
+                />
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      tabIndex={-1}
+                      onClick={() => this.handleToggle(22)}
+                      checked={
+                        this.state.checked.indexOf(22) !== -1 ? true : false
+                      }
+                      checkedIcon={<Check className={classes.checkedIcon} />}
+                      icon={<Check className={classes.uncheckedIcon} />}
+                      classes={{ checked: classes.checked }}
+                    />
+                  }
+                  classes={{ label: classes.label }}
+                  label="Checked"
+                />
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  disabled
+                  control={
+                    <Checkbox
+                      tabIndex={-1}
+                      checkedIcon={<Check className={classes.checkedIcon} />}
+                      icon={<Check className={classes.uncheckedIcon} />}
+                      classes={{ checked: classes.checked }}
+                    />
+                  }
+                  classes={{
+                    label: classes.label,
+                    disabled: classes.disabledCheckboxAndRadio
+                  }}
+                  label="Disabled Unchecked"
+                />
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  disabled
+                  control={
+                    <Checkbox
+                      tabIndex={-1}
+                      checked={
+                        this.state.checked.indexOf(24) !== -1 ? true : false
+                      }
+                      checkedIcon={<Check className={classes.checkedIcon} />}
+                      icon={<Check className={classes.uncheckedIcon} />}
+                      classes={{ checked: classes.checked }}
+                    />
+                  }
+                  classes={{
+                    label: classes.label,
+                    disabled: classes.disabledCheckboxAndRadio
+                  }}
+                  label="Disabled Checked"
+                />
+              </div>
+            </GridItem>
+            <GridItem xs={12} sm={6} md={4} lg={3}>
+              <div className={classes.title}>
+                <h3>Radio Buttons</h3>
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  control={
+                    <Radio
+                      checked={this.state.selectedEnabled === "a"}
+                      onChange={this.handleChangeEnabled}
+                      value="a"
+                      name="radio button enabled"
+                      aria-label="A"
+                      icon={
+                        <FiberManualRecord className={classes.radioUnchecked} />
+                      }
+                      checkedIcon={
+                        <FiberManualRecord className={classes.radioChecked} />
+                      }
+                      classes={{
+                        checked: classes.radio
+                      }}
+                    />
+                  }
+                  classes={{
+                    label: classes.label
+                  }}
+                  label="First Radio"
+                />
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  control={
+                    <Radio
+                      checked={this.state.selectedEnabled === "b"}
+                      onChange={this.handleChangeEnabled}
+                      value="b"
+                      name="radio button enabled"
+                      aria-label="B"
+                      icon={
+                        <FiberManualRecord className={classes.radioUnchecked} />
+                      }
+                      checkedIcon={
+                        <FiberManualRecord className={classes.radioChecked} />
+                      }
+                      classes={{
+                        checked: classes.radio
+                      }}
+                    />
+                  }
+                  classes={{
+                    label: classes.label
+                  }}
+                  label="Second Radio"
+                />
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  disabled
+                  control={
+                    <Radio
+                      checked={false}
+                      value="a"
+                      name="radio button disabled"
+                      aria-label="B"
+                      icon={
+                        <FiberManualRecord className={classes.radioUnchecked} />
+                      }
+                      checkedIcon={
+                        <FiberManualRecord className={classes.radioChecked} />
+                      }
+                      classes={{
+                        checked: classes.radio,
+                        disabled: classes.disabledCheckboxAndRadio
+                      }}
+                    />
+                  }
+                  classes={{
+                    label: classes.label
+                  }}
+                  label="Disabled Unchecked Radio"
+                />
+              </div>
+              <div
+                className={
+                  classes.checkboxAndRadio +
+                  " " +
+                  classes.checkboxAndRadioHorizontal
+                }
+              >
+                <FormControlLabel
+                  disabled
+                  control={
+                    <Radio
+                      checked={true}
+                      value="b"
+                      name="radio button disabled"
+                      aria-label="B"
+                      icon={
+                        <FiberManualRecord className={classes.radioUnchecked} />
+                      }
+                      checkedIcon={
+                        <FiberManualRecord className={classes.radioChecked} />
+                      }
+                      classes={{
+                        checked: classes.radio,
+                        disabled: classes.disabledCheckboxAndRadio
+                      }}
+                    />
+                  }
+                  classes={{ label: classes.label }}
+                  label="Disabled Checked Radio"
+                />
+              </div>
+            </GridItem>
+            <GridItem xs={12} sm={6} md={4} lg={3}>
+              <div className={classes.title}>
+                <h3>Toggle Buttons</h3>
+              </div>
+              <div>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={this.state.checkedA}
+                      onChange={this.handleChange("checkedA")}
+                      value="checkedA"
+                      classes={{
+                        switchBase: classes.switchBase,
+                        checked: classes.switchChecked,
+                        icon: classes.switchIcon,
+                        iconChecked: classes.switchIconChecked,
+                        bar: classes.switchBar
+                      }}
+                    />
+                  }
+                  classes={{
+                    label: classes.label
+                  }}
+                  label="Toggle is on"
+                />
+              </div>
+              <div>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={this.state.checkedB}
+                      onChange={this.handleChange("checkedB")}
+                      value="checkedB"
+                      classes={{
+                        switchBase: classes.switchBase,
+                        checked: classes.switchChecked,
+                        icon: classes.switchIcon,
+                        iconChecked: classes.switchIconChecked,
+                        bar: classes.switchBar
+                      }}
+                    />
+                  }
+                  classes={{
+                    label: classes.label
+                  }}
+                  label="Toggle is off"
+                />
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(demoStyle)(CheckboxRadioSwitchDemo);
