c5e20bd80874f5ba4092a29afb8ec20c773bd170
Mir Sujat <mir.sujat@gmail.com>
2018-09-08 20:22:17 +0600

    Contact page done

--- mui-custom-theme/src/views/Contact/Contact.jsx
+++ mui-custom-theme/src/views/Contact/Contact.jsx
@@ -1,7 +1,143 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const contact = props => {
-  return <div>Contact</div>;
+//Material-ui core component
+import withStyles from "@material-ui/core/styles/withStyles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import GridContainer from "../../components/Grid/GridContainer.jsx";
+import GridItem from "../../components/Grid/GridItem.jsx";
+import Card from "../../components/Card/Card.jsx";
+import CardBody from "../../components/Card/CardBody/CardBody.jsx";
+import CardFooter from "../../components/Card/CardFooter/CardFooter.jsx";
+import Button from "../../components/Button/button.jsx";
+import InputDefault from "../../components/InputDefault/InputDefault.jsx";
+import TextArea from "../../components/InputDefault/TextArea.jsx";
+import inputDefaultStyle from "../../components/InputDefault/inputDefaultStyle.jsx";
+import contactStyle from "./contactStyle.jsx";
+
+const styles = {
+  ...inputDefaultStyle,
+  ...contactStyle
 };
 
-export default contact;
+const contact = props => {
+  const { classes } = props;
+  return (
+    <Fragment>
+      <section>
+        <h1>Contact Form</h1>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={3} />
+          <GridItem xs={12} sm={12} md={6}>
+            <Card className={classes.card}>
+              <CardBody>
+                <h2 className={classes.contactTitle}>Naam Contact Form</h2>
+                <h5 className={classes.contactSubtitle}>
+                  Contact us for custom quote
+                </h5>
+                <InputDefault
+                  id="username"
+                  inputRootCustomClasses={classes.inputRootCustomClasses}
+                  formControlProps={{ className: classes.formControl }}
+                  inputProps={{
+                    placeholder: "User Name",
+                    type: "text",
+                    disableUnderline: true,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <span className={classes.inputLable}>User Name: </span>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+
+                <InputDefault
+                  id="email"
+                  inputRootCustomClasses={classes.inputRootCustomClasses}
+                  formControlProps={{ className: classes.formControl }}
+                  inputProps={{
+                    placeholder: "example@gmail.com",
+                    type: "email",
+                    disableUnderline: true,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <span className={classes.inputLable}>Email: </span>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+
+                <InputDefault
+                  id="password"
+                  inputRootCustomClasses={classes.inputRootCustomClasses}
+                  formControlProps={{ className: classes.formControl }}
+                  inputProps={{
+                    placeholder: "Password",
+                    type: "password",
+                    disableUnderline: true,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <span className={classes.inputLable}>Password: </span>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <InputDefault
+                  id="phone"
+                  inputRootCustomClasses={classes.inputRootCustomClasses}
+                  formControlProps={{ className: classes.formControl }}
+                  inputProps={{
+                    placeholder: "Phone Number",
+                    type: "phone",
+                    disableUnderline: true,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <span className={classes.inputLable}>Phone: </span>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <InputDefault
+                  id="webaddress"
+                  inputRootCustomClasses={classes.inputRootCustomClasses}
+                  formControlProps={{ className: classes.formControl }}
+                  inputProps={{
+                    placeholder: "Your Web Site",
+                    type: "text",
+                    disableUnderline: true,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <span className={classes.inputLable}>Web Site: </span>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <TextArea
+                  id="comment"
+                  inputRootCustomClasses={classes.inputRootCustomClasses}
+                  formControlProps={{ className: classes.formControlTextArea }}
+                  inputProps={{
+                    placeholder: "Comment...",
+                    tyep: "text",
+                    disableUnderline: true,
+                    multiline: true,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <span className={classes.inputLable}>Comment: </span>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <div className={classes.submitButton}>
+                  <Button color="success">Submit</Button>
+                </div>
+              </CardBody>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={3} />
+        </GridContainer>
+      </section>
+    </Fragment>
+  );
+};
+
+export default withStyles(styles)(contact);
---
+++ mui-custom-theme/src/views/Contact/contactStyle.jsx
@@ -0,0 +1,17 @@
+const contactStyle = {
+  submitButton: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "end"
+  },
+  contactTitle: {
+    textAlign: "center",
+    fontWeight: "700"
+  },
+  contactSubtitle: {
+    textAlign: "center",
+    fontWeight: "300"
+  }
+};
+
+export default contactStyle;
