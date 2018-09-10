e0ab2c11edaf2e5e3959876b3b59400548bc9a02
Mir Sujat <mir.sujat@gmail.com>
2018-09-10 15:39:01 +0600

    mirsujat

--- burger-builder/src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Style from './Backdrop.css';
-
-const backdrop = props =>
-	props.show ? <div className={Style.Backdrop} onClick={props.clicked} /> : null;
-
-export default backdrop;
