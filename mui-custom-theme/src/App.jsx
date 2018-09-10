f7ea34e774c1ab3bf437a0d6ce3801aa13d1f70b
Mir Sujat <mir.sujat@gmail.com>
2018-09-10 21:55:19 +0600

    mirsujat

--- mui-custom-theme/src/App.jsx
+++ mui-custom-theme/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 //Views
 import Components from "./views/Components/Components.jsx";
@@ -14,15 +14,13 @@ import Layout from "./HOC/Layout.jsx";
 class App extends Component {
   render() {
     let routes = (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Components} />
-          <Route exact path="/blog" component={Blog} />
-          <Route path="/blog/item" component={BlogItem} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/portfolio" component={Portfolio} />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Components} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/blog/item" component={BlogItem} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/portfolio" component={Portfolio} />
+      </Switch>
     );
     return (
       <div>
