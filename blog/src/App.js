aa43e3fbcf218e1118e9c6dd5ca5ee5e5db4913a
Mir Sujat <mir.sujat@gmail.com>
2018-10-17 19:44:46 +0600

    something

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import Blog from './containers/Blog/Blog';
+import Blog from "./containers/Blog/Blog";
 
 class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<div className="App">
-					<Blog />
-				</div>
-			</BrowserRouter>
-		);
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
