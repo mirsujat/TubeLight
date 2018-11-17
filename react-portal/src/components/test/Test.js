6e067c4891b7f7796973f8381c856f63d68e0c97
Mir Sujat <mir.sujat@gmail.com>
2018-11-17 12:31:56 +0600

    mir

---
+++ react-portal/src/components/test/Test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      This is From Test file.
+      <div>World!</div>
+      <div>World!</div>
+      <div>World!</div>
+    </div>
+  );
+};
+
+export default Test;
