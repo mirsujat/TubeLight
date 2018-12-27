bf92ee4bb161e0c472c352639d68d931ebe9bc33
Mir Sujat <mir.sujat@gmail.com>
2018-12-27 19:42:44 +0600

    mir

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -4,8 +4,35 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    data: { ...Products }
+    data: { ...Products },
+    selectId: null,
+    order: null
   };
+  //! TODO
+  // method-1: select a product to buy
+  // method-2: process the order as user specified
+  // method-3: submit order and added to cart
+  // method-4: calculate total price from ordered products
+  // method-5: handle modal open and close
+  // method-6: handle cart open and close
+  // method-7: validate user input
+  // method-8: remove order from cart
+
+  //! select product by id
+  selectId = id => {
+    const { data } = this.state;
+    const selectId = data.products
+      .filter(product => product.id === id)
+      .reduce((obj, item) => {
+        obj = item;
+        obj.orderNumber = Date.now().toFixed();
+        obj.amount = obj.price * obj.orderQuantity;
+        return obj;
+      }, {});
+
+    this.setState({ selectId: selectId, order: selectId });
+  };
+
   render() {
     const { data } = this.state;
     let content = <div>There is no data to show.</div>;
@@ -17,7 +44,7 @@ class App extends Component {
             <div>Description: {product.description}</div>
             <div>AvailableSizes: {product.availableSizes}</div>
             <div>Price: {product.price}</div>
-            <button>Buy Now</button>
+            <button onClick={() => this.selectId(product.id)}>Buy Now</button>
           </div>
         );
       });
