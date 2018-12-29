f6db0f01f35e78586ede866f876e6ad9e7e70eee
Mir Sujat <mir.sujat@gmail.com>
2018-12-29 10:49:54 +0600

    mir

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -4,10 +4,27 @@ import Products from "./products";
 import "./App.css";
 class App extends Component {
   state = {
-    data: { ...Products }
+    data: { ...Products },
+    order: {}
   };
   // Challenges
   // 1- Display all products -DONE
+  // 2- Select a product to buy or order to buy -DONE
+
+  //! select a product to order
+  selectById = id => {
+    const { data } = this.state;
+    const selectedProduct = data.products
+      .filter(product => product.id === id)
+      .reduce((obj, item) => {
+        obj = item;
+        obj.orderNumber = Date.now().toFixed();
+        obj.amount = obj.price * obj.orderQuantity;
+        return obj;
+      }, {});
+    this.setState({ order: selectedProduct });
+  };
+
   render() {
     const { data } = this.state;
     let content = <div>There is no product to display.</div>;
@@ -21,15 +38,17 @@ class App extends Component {
             <p>
               Price: {product.currencyFormat} {product.price}
             </p>
+            <button onClick={() => this.selectById(product.id)}>Buy Now</button>
           </div>
         );
       });
     }
+
     return (
       <div className="app">
         <div className="header">
           <div>
-            <i class="fas fa-cart-plus" />
+            <i className="fas fa-cart-plus" />
             <span>0</span>
           </div>
         </div>
