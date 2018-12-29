29502c257cb3770836ca403ac96f7581cf7726bd
Mir Sujat <mir.sujat@gmail.com>
2018-12-29 18:59:27 +0600

    mir

---
+++ code-challenge/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="app">
+        <h1>React Code Challenge</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
