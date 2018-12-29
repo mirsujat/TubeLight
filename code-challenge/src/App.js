1cb8b12dfe1b65ebf7c72a2d5c185a93c5c40df6
Mir Sujat <mir.sujat@gmail.com>
2018-12-29 10:05:08 +0600

    mir

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
+import Products from "./products";
+
+import "./App.css";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    data: { ...Products }
+  };
+  // Challenges
+  // 1- Display all products -DONE
   render() {
+    const { data } = this.state;
+    let content = <div>There is no product to display.</div>;
+    if (data) {
+      content = data.products.map(product => {
+        return (
+          <div className="card product" key={product.id}>
+            <p>Name: {product.title}</p>
+            <p>Description: {product.description}</p>
+            <p>Available Sizes: {product.availableSizes}</p>
+            <p>
+              Price: {product.currencyFormat} {product.price}
+            </p>
+          </div>
+        );
+      });
+    }
     return (
       <div className="app">
-        <h1>React Challenge</h1>
+        <div className="header">
+          <div>
+            <i class="fas fa-cart-plus" />
+            <span>0</span>
+          </div>
+        </div>
+        <div className="products">{content}</div>
       </div>
     );
   }
