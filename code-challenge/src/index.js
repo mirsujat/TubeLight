f27b8fd46733bb5b4f40b93f454d86b79eb54423
Mir Sujat <mir.sujat@gmail.com>
2018-12-31 18:43:08 +0600

    mir

--- code-challenge/src/index.js
+++ code-challenge/src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Navigation from "./components/Nav/Nav";
 import App from "./App";
+import Ecom from "./components/Ecom/Ecom";
+import Register from "./components/Register/Register";
 
 import "./index.css";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const AppRoot = () => {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Route path="/ecom" component={Ecom} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(<AppRoot />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
