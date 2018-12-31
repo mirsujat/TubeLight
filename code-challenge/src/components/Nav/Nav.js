1787701fb8467e4ac2e625193e51ed06c18948df
Mir Sujat <mir.sujat@gmail.com>
2018-12-31 15:10:07 +0600

    mir

---
+++ code-challenge/src/components/Nav/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Navigation = () => {
+  return (
+    <div className="header">
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/ecom">Ecom</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
