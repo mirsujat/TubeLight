2a86faaf9a293ea3d61878e92c07506a47187f63
Mir Sujat <mir.sujat@gmail.com>
2019-01-02 12:01:10 +0600

    mir

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -1,77 +1,11 @@
 import React, { Component } from "react";
-import Axios from "axios";
+
 class App extends Component {
-  state = {
-    searchTerm: "me",
-    user: ""
-  };
-  componentDidMount = () => {
-    const { user } = this.state;
-    if (!user) {
-      this.searchIP();
-    }
-  };
-  componentDidUpdate = async (prevProps, prevState) => {
-    const { searchTerm } = this.state;
-    if (
-      prevState.searchTerm !== "me" &&
-      prevState.searchTerm !== searchTerm &&
-      searchTerm.length === 14
-    ) {
-      const { data } = await Axios.get(`https://ip.nf/${searchTerm}.json`);
-      this.setState({ user: data, searchTerm: data.ip.ip });
-    }
-  };
-  handleSubmit = e => {
-    e.preventDefault();
-    this.searchIP();
-  };
-  searchIP = async () => {
-    const { searchTerm } = this.state;
-    const { data } = await Axios.get(`https://ip.nf/${searchTerm}.json`);
-    this.setState({ user: data, searchTerm: data.ip.ip });
-  };
+  state = {};
   render() {
-    const { user } = this.state;
-    console.log("user: ", this.state.user);
-    let content = <div>Loading...</div>;
-    if (user) {
-      content = (
-        <div className="ip-address">
-          <ul>
-            <li>
-              <span>IP Address: </span> {user.ip.ip}
-            </li>
-            <li>
-              <span>Hostname: </span> {user.ip.hostname}
-            </li>
-            <li>
-              <span>City: </span> {user.ip.city}
-            </li>
-            <li>
-              <span>Country: </span> {user.ip.country}
-            </li>
-          </ul>
-        </div>
-      );
-    }
     return (
       <div className="app">
-        <h1>SEARCH YOUR IP ADDRESS.</h1>
-        <div className="search-bar">
-          <form onSubmit={this.handleSubmit} className="search-form">
-            <input
-              className="search"
-              name="search"
-              value={this.state.searchTerm}
-              onChange={e => this.setState({ searchTerm: e.target.value })}
-            />
-            <button type="submit" className="search-btn">
-              Submit
-            </button>
-          </form>
-        </div>
-        {content}
+        <h1>React Challenge</h1>
       </div>
     );
   }
