778cd14d04a494e93079707d76509233d8aec238
Mir Sujat <mir.sujat@gmail.com>
2019-01-05 15:18:26 +0600

    mir

--- code-challenge/src/components/Ecom/Ecom.js
+++ code-challenge/src/components/Ecom/Ecom.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import Products from "../../products";
 
 class Ecom extends Component {
-  state = {};
+  state = {
+    data: { ...Products }
+  };
   render() {
-    return (
-      <div>
-        <h1>Ecommerce</h1>
-      </div>
-    );
+    const { data } = this.state;
+    let content = <div>Loading...</div>;
+    if (data) {
+      content = data.products.map(product => {
+        return (
+          <div className="card product" key={product.id}>
+            <p>Name: {product.title}</p>
+            <p>Description: {product.description}</p>
+            <p>Available Sizes: {product.availavleSizes}</p>
+            <p>
+              Price: {product.currencyFormat} {product.price}
+            </p>
+            <button>BUY NOW</button>
+          </div>
+        );
+      });
+    }
+    return <div className="e-com">{content}</div>;
   }
 }
 
