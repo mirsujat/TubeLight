7893e390615e754823176135510a6d0e18678d77
Mir Sujat <mir.sujat@gmail.com>
2019-02-16 18:53:16 +0600

    mir

--- code-challenge/src/index.js
+++ code-challenge/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
@@ -8,7 +9,13 @@ import "./App.css";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
