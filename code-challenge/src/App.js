3a32209b3f050f2a8a2c9d0044c034dc2e2e516a
Mir Sujat <mir.sujat@gmail.com>
2019-02-19 18:57:06 +0600

    mir

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./Demo-1/Layout/Layout";
 import Home from "./Demo-1/Pages/Home";
 import Contact from "./Demo-1/Pages/Contact";
+import ProductList from "./Demo-1/Pages/Ecom/Products/Products";
+import Cart from "./Demo-1/Pages/Ecom/Cart/Cart";
 
 class App extends Component {
   state = {};
@@ -12,6 +14,8 @@ class App extends Component {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/contact" exact component={Contact} />
+        <Route path="/products" exact component={ProductList} />
+        <Route path="/cart" exact component={Cart} />
       </Switch>
     );
     return (
