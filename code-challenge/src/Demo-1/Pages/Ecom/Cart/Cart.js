c497f47b98b535f7a039ece9fa6ef7ec51977a5e
Mir Sujat <mir.sujat@gmail.com>
2019-02-19 18:57:43 +0600

    mir

---
+++ code-challenge/src/Demo-1/Pages/Ecom/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Cart extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Cart: Display Shopping Cart</h1>
+      </div>
+    );
+  }
+}
+
+export default Cart;
