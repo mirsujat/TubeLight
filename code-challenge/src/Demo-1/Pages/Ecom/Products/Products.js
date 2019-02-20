1880e5c03beb6a9381bbe2bfde5537c90f4bc9c9
Mir Sujat <mir.sujat@gmail.com>
2019-02-20 19:02:15 +0600

    mir

--- code-challenge/src/Demo-1/Pages/Ecom/Products/Products.js
+++ code-challenge/src/Demo-1/Pages/Ecom/Products/Products.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import products from "../../../../products";
 import ListItem from "../ListItem/ListItem";
+import Cart from "../Cart/Cart";
 
 class ProductList extends Component {
   state = {
-    data: { ...products }
+    data: { ...products },
+    cart: [],
+    cartopen: false
+  };
+
+  handleBuyNow = id => {
+    const { data, cart } = this.state;
+    const cartItem = data.products.filter(product => {
+      return product.sku === id;
+    });
+    cart.push(cartItem);
+    this.setState({ cart });
+  };
+  handleCartOpen = () => {
+    this.setState(prevState => {
+      return { cartopen: !prevState.cartopen };
+    });
   };
   render() {
-    const { data } = this.state;
+    const { data, cart, cartopen } = this.state;
+    console.log("cart: ", cart);
     let lists = <div>Loading...</div>;
-    console.log(data);
+    let shoppingCart = null;
     if (data) {
       lists = data.products.map(product => {
         return (
@@ -20,11 +38,53 @@ class ProductList extends Component {
             size={product.availableSizes}
             currencyFormate={product.currencyFormat}
             price={product.price}
+            clicked={() => this.handleBuyNow(product.sku)}
           />
         );
       });
     }
-    return <div className="products">{lists}</div>;
+    if (cart) {
+      shoppingCart = (
+        <Cart open={this.state.cartopen} cartclicked={this.handleCartOpen}>
+          <table>
+            <thead>
+              <tr>
+                <td>#</td>
+                <td>Order Id</td>
+                <td>Product Name</td>
+                <td>Product Description</td>
+                <td>Size</td>
+                <td>Unit Price($)</td>
+                <td>Amount($)</td>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item, index) => {
+                return (
+                  <tr key={index + 1}>
+                    <td>{index + 1}</td>
+                    <td>{item.sku}</td>
+                    <td>{item.title}</td>
+                    <td>{item.description}</td>
+                    <td>{item.size}</td>
+                    <td>
+                      {item.currencyFormat} {item.price}
+                    </td>
+                    <td>{item.amount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Cart>
+      );
+    }
+    return (
+      <div className="products">
+        {lists}
+        {shoppingCart}
+      </div>
+    );
   }
 }
 
