e5b9bd9cf9e4c3a9224f1a8fdfe62371be29dab2
Mir Sujat <mir.sujat@gmail.com>
2019-03-22 19:27:35 +0600

    mir

--- new-challenge/src/index.js
+++ new-challenge/src/index.js
@@ -13,16 +13,22 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => alert("click")}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
