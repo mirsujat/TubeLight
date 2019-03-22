869badd5985d219912712799609e0394af0b319d
Mir Sujat <mir.sujat@gmail.com>
2019-03-22 19:46:42 +0600

    Update index.js

--- new-challenge/src/index.js
+++ new-challenge/src/index.js
@@ -5,16 +5,10 @@ import "./App.css";
 import * as serviceWorker from "./serviceWorker";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -27,8 +21,19 @@ class Board extends React.Component {
       squares: Array(9).fill(null)
     };
   }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
