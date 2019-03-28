42a67e2bd0f3dfbe33c3fdcfd84cae7b15d6bc10
Mir Sujat <mir.sujat@gmail.com>
2019-03-28 20:50:34 +0600

    refactor board component

--- new-challenge/src/index.js
+++ new-challenge/src/index.js
@@ -11,39 +11,53 @@ const Square = props => {
     </button>
   );
 };
+// class Board extends React.Component {
+//   renderSquare(i) {
+//     return (
+//       <Square
+//         value={this.props.squares[i]}
+//         onClick={() => this.props.onClick(i)}
+//       />
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
 class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
+  state = {
+    row: [0, 1, 2],
+    squares: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  };
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    let renderSquares = null;
+    let boardRow = null;
+    let boardSquare = null;
+
+    return <div>{renderSquares}</div>;
   }
 }
-
 class Game extends React.Component {
   constructor(props) {
     super(props);
