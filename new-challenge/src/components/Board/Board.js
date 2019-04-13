2d7a3ac37f985d8fe4390f5535f2d1950e674611
Mir Sujat <mir.sujat@gmail.com>
2019-04-13 22:08:09 +0600

    implement board with loop

--- new-challenge/src/components/Board/Board.js
+++ new-challenge/src/components/Board/Board.js
@@ -6,29 +6,24 @@ class Board extends Component {
     squares: Array(9).fill(null)
   };
   renderSquares(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square key={i} value={this.state.squares[i]} />;
   }
 
   render() {
-    return (
-      <div className="board">
-        <div className="board-row">
-          {this.renderSquares(0)}
-          {this.renderSquares(1)}
-          {this.renderSquares(2)}
+    const boardSize = 3;
+    let squares = [];
+    for (let i = 0; i < boardSize; ++i) {
+      let row = [];
+      for (let j = 0; j < boardSize; ++j) {
+        row.push(this.renderSquares(i * boardSize + j));
+      }
+      squares.push(
+        <div key={i} className="board-row">
+          {row}
         </div>
-        <div className="board-row">
-          {this.renderSquares(3)}
-          {this.renderSquares(4)}
-          {this.renderSquares(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquares(6)}
-          {this.renderSquares(7)}
-          {this.renderSquares(8)}
-        </div>
-      </div>
-    );
+      );
+    }
+    return <div className="board">{squares}</div>;
   }
 }
 
