b2849a3f772e5b9396de4cc49925ee186dd36115
Mir Sujat <mir.sujat@gmail.com>
2019-04-13 21:57:14 +0600

    Update Game.js

--- new-challenge/src/components/Game/Game.js
+++ new-challenge/src/components/Game/Game.js
@@ -1,1 +1,15 @@
 import React, { Component } from "react";
+import Board from "../Board/Board";
+
+class Game extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="game">
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default Game;
