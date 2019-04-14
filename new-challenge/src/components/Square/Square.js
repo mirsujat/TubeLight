47415634a8dc8f62da3eefc1cef22eed2e2ae608
Mir Sujat <mir.sujat@gmail.com>
2019-04-14 12:38:56 +0600

    Update Square.js

--- new-challenge/src/components/Square/Square.js
+++ new-challenge/src/components/Square/Square.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Square = props => <button className="square">{props.value}</button>;
+const Square = props => (
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+  </button>
+);
 
 export default Square;
