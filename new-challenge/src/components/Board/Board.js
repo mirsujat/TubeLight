be62250536e2a6788cefcbc01b9198388a217aa3
Mir Sujat <mir.sujat@gmail.com>
2019-04-14 15:03:46 +0600

    Update Board.js

--- new-challenge/src/components/Board/Board.js
+++ new-challenge/src/components/Board/Board.js
@@ -18,6 +18,9 @@ class Board extends Component {
   };
   handleClick = i => {
     const { squares } = this.state;
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({ squares, xIsNext: !this.state.xIsNext });
   };
@@ -36,8 +39,40 @@ class Board extends Component {
         </div>
       );
     }
-    return <div className="board">{squares}</div>;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner : " + winner;
+    } else {
+      status = "Next Player: " + (this.state.xIsNext ? " X" : "O");
+    }
+    return (
+      <div className="board">
+        <div className="status">{status}</div>
+        {squares}
+      </div>
+    );
   }
 }
 
 export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
