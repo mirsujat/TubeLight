bb4a40abbb5680134990a97c067f497354169d67
Mir Sujat <mir.sujat@gmail.com>
2019-04-18 21:01:20 +0600

    format data

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -19,7 +19,14 @@ class Products {
     try {
       let result = await fetch("products.json");
       let data = await result.json();
-      return data;
+      let products = data.items;
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {
       console.log(error);
     }
