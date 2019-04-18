1b343411bd289bfd367dfdb9014cc262e438b522
Mir Sujat <mir.sujat@gmail.com>
2019-04-18 19:57:22 +0600

    add class and vars

---
+++ shopping-cart/app.js
@@ -0,0 +1,28 @@
+// variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productDOM = document.querySelector(".products-center");
+
+// Cart
+let cart = [];
+
+// Getting the products
+class Products {}
+
+// Display Product
+class UI {}
+
+// local Storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
