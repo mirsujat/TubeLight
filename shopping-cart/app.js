525682a72476314d23b2076a0491f6aed7e36738
Mir Sujat <mir.sujat@gmail.com>
2019-04-19 15:53:00 +0600

    setup local storage

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -63,11 +63,18 @@ class UI {
 }
 
 // local Storage
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
 
-  products.getProducts().then(products => ui.displayProducts(products));
+  products.getProducts().then(products => {
+    ui.displayProducts(products);
+    Storage.saveProducts(products);
+  });
 });
