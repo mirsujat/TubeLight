96eda3ae719cdd2df149825dfd440d06d4d07907
Mir Sujat <mir.sujat@gmail.com>
2019-04-21 20:15:05 +0600

    title component added

--- react-net-store/src/components/ProductList/ProductList.js
+++ react-net-store/src/components/ProductList/ProductList.js
@@ -1,13 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Product from "../Product/Product";
+import Title from "../Title/Title";
 
 class ProductList extends Component {
-  state = {};
+  state = {
+    products: []
+  };
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <Frament>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row" />
+          </div>
+        </div>
+      </Frament>
+      //<Product />
     );
   }
 }
