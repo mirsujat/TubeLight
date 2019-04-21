fd66d92597e1b409853de64447fb60b5907f7d6b
Mir Sujat <mir.sujat@gmail.com>
2019-04-21 16:03:05 +0600

    Update Navbar.js

--- react-net-store/src/components/Navbar/Navbar.js
+++ react-net-store/src/components/Navbar/Navbar.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../logo.svg";
 
 class Navbar extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h3>Hello from Navbar</h3>
-      </div>
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk  
+*/}
+        <Link to="/">
+          <img src={logo} className="navbar-brand" alt="logo" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button className="">
+            <i className="fas fa-cart-plus" />
+            my cart
+          </button>
+        </Link>
+      </nav>
     );
   }
 }
