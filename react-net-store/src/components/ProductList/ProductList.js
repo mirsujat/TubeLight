15b3f5e27fda750bc58389650d2f085b2176a011
Mir Sujat <mir.sujat@gmail.com>
2019-04-21 20:30:06 +0600

    mir

--- react-net-store/src/components/ProductList/ProductList.js
+++ react-net-store/src/components/ProductList/ProductList.js
@@ -1,12 +1,16 @@
 import React, { Component, Fragment } from "react";
+import { storeProducts } from "../../data";
 import Product from "../Product/Product";
 import Title from "../Title/Title";
 
 class ProductList extends Component {
   state = {
-    products: []
+    products: storeProducts
   };
   render() {
+    console.log("====================================");
+    console.log(this.state.products);
+    console.log("====================================");
     return (
       <Fragment>
         <div className="py-5">
