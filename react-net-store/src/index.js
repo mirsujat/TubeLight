322381eedeaedfc04ae4e0fbd24f31ee5f7a9a14
Mir Sujat <mir.sujat@gmail.com>
2019-04-22 14:51:33 +0600

    Update index.js

--- react-net-store/src/index.js
+++ react-net-store/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { ProductProvider } from "./components/context/context";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   document.getElementById("root")
 );
 
