d84922bd459358993b9345a7d64238f9c63a7f22
Mir Sujat <mir.sujat@gmail.com>
2019-04-22 15:54:06 +0600

    Update Product.js

--- react-net-store/src/components/Product/Product.js
+++ react-net-store/src/components/Product/Product.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context/context";
 
 class Product extends Component {
   state = {};
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h3>Hello from product page</h3>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => console.log("You Clicked on image container")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product img" className="card-img-top" />
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
 
 export default Product;
