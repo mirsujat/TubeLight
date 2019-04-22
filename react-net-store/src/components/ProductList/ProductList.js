5158b391beaa0c14f1ba1c1cf8132d321d72193b
Mir Sujat <mir.sujat@gmail.com>
2019-04-22 14:51:13 +0600

    Update ProductList.js

--- react-net-store/src/components/ProductList/ProductList.js
+++ react-net-store/src/components/ProductList/ProductList.js
@@ -2,21 +2,25 @@ import React, { Component, Fragment } from "react";
 import { storeProducts } from "../../data";
 import Product from "../Product/Product";
 import Title from "../Title/Title";
+import { ProductConsumer } from "../context/context";
 
 class ProductList extends Component {
   state = {
     products: storeProducts
   };
   render() {
-    console.log("====================================");
-    console.log(this.state.products);
-    console.log("====================================");
     return (
       <Fragment>
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return <h1>{value}</h1>;
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </Fragment>
