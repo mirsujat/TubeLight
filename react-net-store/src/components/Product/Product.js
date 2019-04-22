1024cadc4c92f75c09e7acc6a9b910ffd487bfc2
Mir Sujat <mir.sujat@gmail.com>
2019-04-22 20:28:11 +0600

    add style

--- react-net-store/src/components/Product/Product.js
+++ react-net-store/src/components/Product/Product.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context/context";
 
@@ -27,9 +28,16 @@ class Product extends Component {
                   in Cart
                 </p>
               ) : (
-                <i className="fas fa-cart-plus" />
+                <i className="fas fa-cart-plus">cart</i>
               )}
             </button>
+          </div>
+          {/* cart footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="rigth-one">${price}</span>
+            </h5>
           </div>
         </div>
       </ProductWrapper>
@@ -37,6 +45,65 @@ class Product extends Component {
   }
 }
 
-const ProductWrapper = styled.div``;
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool
+  }).isRequired
+};
+
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 500ms linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 500ms linear;
+  }
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+  .card-img-top {
+    transition: all 500ms linear;
+  }
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 500ms linear;
+  }
+  .img-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+  .cart-btn:hover {
+    color: var(--mainBlue);
+    cursor: pointer;
+  }
+`;
 
 export default Product;
