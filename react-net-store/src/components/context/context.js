01aed35b6a66b0a2699c2eb0892479c2d7095f37
Mir Sujat <mir.sujat@gmail.com>
2019-04-22 20:27:50 +0600

    fix data immutability

--- react-net-store/src/components/context/context.js
+++ react-net-store/src/components/context/context.js
@@ -8,8 +8,21 @@ const ProductContext = createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct
+  };
+  componentDidMount() {
+    this.setProducts();
+  }
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
   };
   handleDetail = () => {
     console.log("====================================");
