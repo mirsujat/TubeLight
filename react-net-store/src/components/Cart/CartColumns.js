048e4f563f7631f09ae831999290e5254832a07e
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 16:58:07 +0600

    Create CartColumns.js

---
+++ react-net-store/src/components/Cart/CartColumns.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CartColumns = () => {
+  return (
+    <div className="container-fluid text-center d-none d-lg-block">
+      <div className="row">
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">products</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">products</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartColumns;
