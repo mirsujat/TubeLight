8fa761c5e1369e41d1a9a389e6535aa759494b6b
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 12:57:29 +0600

    working on modal

--- react-net-store/src/components/context/context.js
+++ react-net-store/src/components/context/context.js
@@ -10,7 +10,9 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct,
-    cart: []
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct
   };
   componentDidMount() {
     this.setProducts();
@@ -53,14 +55,27 @@ class ProductProvider extends Component {
       () => console.log(this.state)
     );
   };
+  openModal = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
 
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
+  };
   render() {
     return (
       <ProductContext.Provider
         value={{
           ...this.state,
           handleDetail: this.handleDetail,
-          addToCart: this.addToCart
+          addToCart: this.addToCart,
+          openModal: this.openModal,
+          closeModal: this.closeModal
         }}
       >
         {this.props.children}
