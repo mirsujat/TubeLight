456e9a333b8f98cfdd96484b8a3963e9be09be7b
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 20:29:00 +0600

    Create CartTotals.js

---
+++ react-net-store/src/components/Cart/CartTotals.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const CartTotals = ({ value }) => {
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+  return (
+    <Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mt-2 mb-sm-5 ml auto col-sm-8 text-capitalize text-right">
+            <Link to="/">
+              <button
+                className="btn btn-danger text-uppercase mb-3 px-5"
+                type="button"
+                onClick={() => clearCart()}
+              >
+                clear cart
+              </button>
+            </Link>
+            <h5>
+              <span className="text-title">subtotal: </span>
+              <strong>$ {cartSubTotal}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">tax: </span>
+              <strong>$ {cartTax}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">total: </span>
+              <strong>$ {cartTotal}</strong>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default CartTotals;
