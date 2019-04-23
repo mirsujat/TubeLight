b166a249a66a92c24a4031c6eec12bbb7ce88c22
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 17:01:11 +0600

    Update CartColumns.js

--- react-net-store/src/components/Cart/CartColumns.js
+++ react-net-store/src/components/Cart/CartColumns.js
@@ -8,19 +8,19 @@ const CartColumns = () => {
           <p className="text-uppercase">products</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">name of products</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">price</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">quantity</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">remove</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">total</p>
         </div>
       </div>
     </div>
