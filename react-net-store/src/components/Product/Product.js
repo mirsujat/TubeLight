9b1614a35ff9da5585ff1ff9822d90947b88dc7f
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 10:59:07 +0600

    handeDetail and addToCart method added

--- react-net-store/src/components/Product/Product.js
+++ react-net-store/src/components/Product/Product.js
@@ -11,27 +11,32 @@ class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container p-5"
-            onClick={() => console.log("You Clicked on image container")}
-          >
-            <Link to="/details">
-              <img src={img} alt="product img" className="card-img-top" />
-            </Link>
-            <button
-              className="cart-btn"
-              disabled={inCart ? true : false}
-              onClick={() => console.log("added to the cart")}
-            >
-              {inCart ? (
-                <p className="text-capitalize mb-0" disabled>
-                  in Cart
-                </p>
-              ) : (
-                <i className="fas fa-cart-plus" />
-              )}
-            </button>
-          </div>
+          <ProductConsumer>
+            {value => (
+              <div
+                className="img-container p-5"
+                onClick={() => value.handleDetail(id)}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product img" className="card-img-top" />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => value.addToCart(id)}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      in Cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
+
           {/* cart footer */}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
