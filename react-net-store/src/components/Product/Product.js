5b7740582f1ab84541503df7b6a16ef78ed7de24
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 12:58:20 +0600

    working on modal

--- react-net-store/src/components/Product/Product.js
+++ react-net-store/src/components/Product/Product.js
@@ -23,7 +23,10 @@ class Product extends Component {
                 <button
                   className="cart-btn"
                   disabled={inCart ? true : false}
-                  onClick={() => value.addToCart(id)}
+                  onClick={() => {
+                    value.addToCart(id);
+                    value.openModal(id);
+                  }}
                 >
                   {inCart ? (
                     <p className="text-capitalize mb-0" disabled>
