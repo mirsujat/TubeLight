05ac5787cf8493a302f30d54baab25621e1c990d
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 15:58:49 +0600

    initialize methods

--- react-net-store/src/components/context/context.js
+++ react-net-store/src/components/context/context.js
@@ -12,7 +12,10 @@ class ProductProvider extends Component {
     detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
   componentDidMount() {
     this.setProducts();
@@ -67,6 +70,19 @@ class ProductProvider extends Component {
       return { modalOpen: false };
     });
   };
+  increment = id => {
+    console.log("This is increment method");
+  };
+  decrement = id => {
+    console.log("This is decrement method");
+  };
+  removeItem = id => {
+    console.log("this is remove item method");
+  };
+  clearCart = () => {
+    console.log("Clear cart method");
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -75,7 +91,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
