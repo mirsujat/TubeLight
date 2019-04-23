4de29290adce85fc3b64d969af7a878719bd83eb
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 17:10:57 +0600

    Update Cart.js

--- react-net-store/src/components/Cart/Cart.js
+++ react-net-store/src/components/Cart/Cart.js
@@ -1,16 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Title from "../Title/Title";
 import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
+import { ProductConsumer } from "../context/context";
 
 class Cart extends Component {
   state = {};
   render() {
     return (
       <section>
-        <Title name="your" title="cart" />
-        <CartColumns />
-        <EmptyCart />
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColumns />
+                </Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
       </section>
     );
   }
