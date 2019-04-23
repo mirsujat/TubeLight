4812ed77b727f70f70a5572a0ce9148b0b1c949a
Mir Sujat <mir.sujat@gmail.com>
2019-04-23 20:28:28 +0600

    Create CartItem.js

---
+++ react-net-store/src/components/Cart/CartItem.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const CartItem = ({ item, value }) => {
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+  return (
+    <div className="row my-2 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img
+          src={img}
+          alt="product"
+          style={{ width: "5rem", height: "5rem" }}
+          className="img-fluid"
+        />
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">product : </span>
+        {title}
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">price : </span>
+        {price}
+      </div>
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex jusity-content-center">
+          <span className="btn btn-black mx-auto" onClick={() => decrement(id)}>
+            -
+          </span>
+          <span className="btn btn-black mx-1">{count}</span>
+          <span className="btn btn-black mx-1" onClick={() => increment(id)}>
+            +
+          </span>
+        </div>
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <div className="cart-icon" onClick={() => removeItem(id)}>
+          <i className="fas fa-trash" />
+        </div>
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <strong> item total: $ {total}</strong>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
