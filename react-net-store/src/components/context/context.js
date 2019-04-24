e1883e639273d2567c961244e3c71dfe14c3f6e2
Mir Sujat <mir.sujat@gmail.com>
2019-04-24 12:43:51 +0600

    increment method implemented

--- react-net-store/src/components/context/context.js
+++ react-net-store/src/components/context/context.js
@@ -73,7 +73,24 @@ class ProductProvider extends Component {
     });
   };
   increment = id => {
-    console.log("This is increment method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
   decrement = id => {
     console.log("This is decrement method");
