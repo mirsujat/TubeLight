1fcb171e3fd22894302b1d2006ce844c5b865dca
Mir Sujat <mir.sujat@gmail.com>
2019-04-24 12:56:05 +0600

    decrement method implemented

--- react-net-store/src/components/context/context.js
+++ react-net-store/src/components/context/context.js
@@ -93,7 +93,28 @@ class ProductProvider extends Component {
     );
   };
   decrement = id => {
-    console.log("This is decrement method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count - 1;
+    if (product.count === 0) {
+      this.removeItem(id);
+    } else {
+      product.total = product.count * product.price;
+      this.setState(
+        () => {
+          return {
+            cart: [...tempCart]
+          };
+        },
+        () => {
+          this.addTotals();
+        }
+      );
+    }
   };
   removeItem = id => {
     let tempProducts = [...this.state.products];
