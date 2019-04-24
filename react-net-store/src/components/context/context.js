875fe004aae1903059b90cca0d8f2a447aa35237
Mir Sujat <mir.sujat@gmail.com>
2019-04-24 12:10:20 +0600

    removeItem method implemented

--- react-net-store/src/components/context/context.js
+++ react-net-store/src/components/context/context.js
@@ -79,7 +79,25 @@ class ProductProvider extends Component {
     console.log("This is decrement method");
   };
   removeItem = id => {
-    console.log("this is remove item method");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
   clearCart = () => {
     this.setState(
