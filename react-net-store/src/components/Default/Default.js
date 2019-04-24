1f50c0b6ad42ac5d222afd556fdf4f9ef24db493
Mir Sujat <mir.sujat@gmail.com>
2019-04-24 13:15:20 +0600

    page 404 added

--- react-net-store/src/components/Default/Default.js
+++ react-net-store/src/components/Default/Default.js
@@ -4,8 +4,21 @@ class Default extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h3>Page Not Found ):</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>error</h1>
+            <h2>page not found</h2>
+            <h1>
+              the requested URL{" "}
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>{" "}
+              was not found
+            </h1>
+          </div>
+        </div>
       </div>
     );
   }
