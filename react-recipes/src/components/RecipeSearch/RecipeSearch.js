8da9aa3b1b8ea1eb04c7b272b9a16ee342632154
Mir Sujat <mir.sujat@gmail.com>
2019-04-25 13:04:06 +0600

    Create RecipeSearch.js

---
+++ react-recipes/src/components/RecipeSearch/RecipeSearch.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class RecipeSearch extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello From Recipe Search</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default RecipeSearch;
