7eb559a6288315006ece6d2d3d50b025e086c279
Mir Sujat <mir.sujat@gmail.com>
2019-04-25 16:13:23 +0600

    Update RecipeList.js

--- react-recipes/src/components/RecipeList/RecipeList.js
+++ react-recipes/src/components/RecipeList/RecipeList.js
@@ -5,11 +5,24 @@ import RecipeSearch from "../RecipeSearch/RecipeSearch";
 class RecipeList extends Component {
   state = {};
   render() {
+    const { recipes } = this.props;
     return (
       <Fragment>
-        <h1>Hello From Recipe List</h1>
         <RecipeSearch />
-        <Recipe />
+        <div className="container my-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+          <div className="row">
+            {recipes.map(recipe => {
+              return <Recipe key={recipe.recipe_id} recipe={recipe} />;
+            })}
+          </div>
+        </div>
       </Fragment>
     );
   }
