2393965e334e90ccb0c2f98ab7d2f5784d26f6a2
Mir Sujat <mir.sujat@gmail.com>
2019-04-25 13:03:53 +0600

    Create RecipeList.js

---
+++ react-recipes/src/components/RecipeList/RecipeList.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class RecipeList extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello From Recipe List</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default RecipeList;
