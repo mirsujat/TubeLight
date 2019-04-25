1392923443133fa877245cdfa20512fd32afb714
Mir Sujat <mir.sujat@gmail.com>
2019-04-25 13:03:39 +0600

    Create RecipeDetails.js

---
+++ react-recipes/src/components/RecipeDetails/RecipeDetails.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class RecipeDetails extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello From Recipe Details</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default RecipeDetails;
