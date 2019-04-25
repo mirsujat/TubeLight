71dbdae3f9e1df62eff5b4ed8dfce430a2723138
Mir Sujat <mir.sujat@gmail.com>
2019-04-25 12:51:35 +0600

    Update App.js

--- react-recipes/src/App.js
+++ react-recipes/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
