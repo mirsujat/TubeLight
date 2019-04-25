6fe4f992e42de46d0a756f87fbcb0c79d8422d33
Mir Sujat <mir.sujat@gmail.com>
2019-04-25 13:03:27 +0600

    Create Recipe.js

---
+++ react-recipes/src/components/Recipe/Recipe.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class Recipe extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello from recipe</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default Recipe;
