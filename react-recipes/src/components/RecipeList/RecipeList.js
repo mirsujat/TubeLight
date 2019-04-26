313199e0091629228763457399c3cf8f5d16c455
Mir Sujat <mir.sujat@gmail.com>
2019-04-26 19:36:46 +0600

    Update RecipeList.js

--- react-recipes/src/components/RecipeList/RecipeList.js
+++ react-recipes/src/components/RecipeList/RecipeList.js
@@ -10,7 +10,8 @@ class RecipeList extends Component {
       handleDetails,
       value,
       handleChange,
-      handleSubmit
+      handleSubmit,
+      error
     } = this.props;
     return (
       <Fragment>
@@ -28,15 +29,19 @@ class RecipeList extends Component {
           </div>
           {/* end of title */}
           <div className="row">
-            {recipes.map(recipe => {
-              return (
-                <Recipe
-                  key={recipe.recipe_id}
-                  recipe={recipe}
-                  handleDetails={handleDetails}
-                />
-              );
-            })}
+            {error ? (
+              <h1 className="text-danger text-center">{error}</h1>
+            ) : (
+              recipes.map(recipe => {
+                return (
+                  <Recipe
+                    key={recipe.recipe_id}
+                    recipe={recipe}
+                    handleDetails={handleDetails}
+                  />
+                );
+              })
+            )}
           </div>
         </div>
       </Fragment>
