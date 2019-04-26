e7adba0899d55b62633522f5e36ad36d6a8e56f3
Mir Sujat <mir.sujat@gmail.com>
2019-04-26 20:05:44 +0600

    Create Footer.js

---
+++ react-recipes/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-center text-slanted">
+      <h2 className="text-uppercase">available for hire</h2>
+      <h6>
+        with <i className="fas fa-heart" /> Mir Sujat, E-mail:
+        mir.sujat@gmail.com{" "}
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
