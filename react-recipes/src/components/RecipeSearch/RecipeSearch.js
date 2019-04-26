35429040b60feab989477db2c37a365919479dba
Mir Sujat <mir.sujat@gmail.com>
2019-04-26 16:12:45 +0600

    Update RecipeSearch.js

--- react-recipes/src/components/RecipeSearch/RecipeSearch.js
+++ react-recipes/src/components/RecipeSearch/RecipeSearch.js
@@ -5,7 +5,37 @@ class RecipeSearch extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Hello From Recipe Search</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+              <h1 className="text-slanted text-capitalize">
+                search for recipe with{" "}
+                <strong className="text-dange">Food2Fork</strong>
+              </h1>
+              <form className="mt-4">
+                <label htmlFor="search" className="text-capitalize">
+                  type recipes seperated by comma
+                </label>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    name="search"
+                    placeholder="chicken, onions, carrots"
+                    className="form-control"
+                  />
+                  <div className="input-group-append">
+                    <button
+                      type="button"
+                      className="input-group-text bg-primary text-white"
+                    >
+                      <i className="fas fa-search" />
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </Fragment>
     );
   }
