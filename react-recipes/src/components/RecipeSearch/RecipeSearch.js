6f02a4a9668ad9341741a8aad5165774c894def2
Mir Sujat <mir.sujat@gmail.com>
2019-04-26 16:39:58 +0600

    Update RecipeSearch.js

--- react-recipes/src/components/RecipeSearch/RecipeSearch.js
+++ react-recipes/src/components/RecipeSearch/RecipeSearch.js
@@ -3,6 +3,7 @@ import React, { Component, Fragment } from "react";
 class RecipeSearch extends Component {
   state = {};
   render() {
+    const { value, handleChange, handleSubmit } = this.props;
     return (
       <Fragment>
         <div className="container">
@@ -10,9 +11,9 @@ class RecipeSearch extends Component {
             <div className="col-10 mx-auto col-md-8 mt-5 text-center">
               <h1 className="text-slanted text-capitalize">
                 search for recipe with{" "}
-                <strong className="text-dange">Food2Fork</strong>
+                <strong className="text-danger">Food2Fork</strong>
               </h1>
-              <form className="mt-4">
+              <form className="mt-4" onSubmit={handleSubmit}>
                 <label htmlFor="search" className="text-capitalize">
                   type recipes seperated by comma
                 </label>
@@ -22,10 +23,12 @@ class RecipeSearch extends Component {
                     name="search"
                     placeholder="chicken, onions, carrots"
                     className="form-control"
+                    value={value}
+                    onChange={handleChange}
                   />
                   <div className="input-group-append">
                     <button
-                      type="button"
+                      type="submit"
                       className="input-group-text bg-primary text-white"
                     >
                       <i className="fas fa-search" />
