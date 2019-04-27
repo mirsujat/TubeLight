a86e236d6f248c7e654f9ebe6cdc781ee081a719
Mir Sujat <mir.sujat@gmail.com>
2019-04-27 15:08:19 +0600

    project finished

--- nextjs-auth/settings.js
+++
@@ -1,7 +0,0 @@
-const clientID = process.env.CLIENTID || '';
-const domain = process.env.DOMAIN || '';
-
-export {
-  clientID,
-  domain
-};
--- redux-saga/src/components/UI/Spinner/Spinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import Style from './Spinner.css';
-
-const spinner = () => <div className={Style.Loader}>Loading...</div>;
-
-export default spinner;
