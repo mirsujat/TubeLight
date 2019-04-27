9263bd439e49fc41678028683d6dbabe4b79c7c3
Mir Sujat <mir.sujat@gmail.com>
2019-04-27 20:56:48 +0600

    Update App.js

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1>Hellow From App</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
       </div>
     );
   }
