4ae45d5d135df659cb5a3b2497aaf669cb342f0a
Mir Sujat <mir.sujat@gmail.com>
2019-04-29 20:24:31 +0600

    Implement removeTour method

--- react-city-tour/src/components/Tour/Tour.js
+++ react-city-tour/src/components/Tour/Tour.js
@@ -11,12 +11,13 @@ class Tour extends Component {
   };
 
   render() {
-    const { city, img, name, info } = this.props.tour;
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
     return (
       <article className="tour">
         <div className="img-container">
           <img src={img} alt="city" />
-          <span className="close-btn">
+          <span className="close-btn" onClick={() => removeTour(id)}>
             <i className="fas fa-window-close" />
           </span>
         </div>
--- react-city-tour/src/components/TourList/TourList.js
+++ react-city-tour/src/components/TourList/TourList.js
@@ -7,12 +7,21 @@ class TourList extends Component {
   state = {
     tours: tourData
   };
+  removeTour = id => {
+    const { tours } = this.state;
+    const sortedTours = tours.filter(tour => tour.id !== id);
+    this.setState({
+      tours: sortedTours
+    });
+  };
   render() {
     const { tours } = this.state;
     return (
       <section className="tourList">
         {tours.map(tour => {
-          return <Tour key={tour.id} tour={tour} />;
+          return (
+            <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+          );
         })}
       </section>
     );
