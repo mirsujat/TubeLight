754e1a55f2861916e8b81696884a49e5c105fe45
Mir Sujat <mir.sujat@gmail.com>
2019-05-14 21:06:18 +0600

    create gameLoop function

--- Brick-Breaker/src/index.js
+++ Brick-Breaker/src/index.js
@@ -12,3 +12,18 @@ ctx.clearRect(0, 0, 800, 600);
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  // clear the screen
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
