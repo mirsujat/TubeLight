40b6e73b87d05aeff3e9df10d20f51eca3bbe231
Mir Sujat <mir.sujat@gmail.com>
2019-05-15 16:05:44 +0600

    Update game.js

--- Brick-Breaker/src/game.js
+++ Brick-Breaker/src/game.js
@@ -9,19 +9,23 @@ export default class Game {
   }
 
   start() {
-    this.paddle = new Paddle(this);
+    // create new instance of Paddle and Ball class and passes it's own this
+    // so that they can get any information from it's own class if need.
     this.ball = new Ball(this);
+    this.paddle = new Paddle(this);
+
+    this.gameObjects = [this.ball, this.paddle];
 
     new InputHandler(this.paddle);
   }
 
+  // update the ball and paddle
   update(deltaTime) {
-    this.paddle.update(deltaTime);
-    this.ball.update(deltaTime);
+    this.gameObjects.forEach(object => object.update(deltaTime));
   }
 
+  // draw the ball and paddle
   draw(ctx) {
-    this.paddle.draw(ctx);
-    this.ball.draw(ctx);
+    this.gameObjects.forEach(object => object.draw(ctx));
   }
 }
