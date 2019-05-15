0d224e25c62cb577ced7a259a8fe59f7fc443d42
Mir Sujat <mir.sujat@gmail.com>
2019-05-15 19:32:11 +0600

    level1 added

--- Brick-Breaker/src/brick.js
+++ Brick-Breaker/src/brick.js
@@ -5,7 +5,7 @@ export default class Brick {
     this.game = game;
 
     this.position = position;
-    this.width = 52;
+    this.width = 80;
     this.height = 24;
   }
   update() {}
--- Brick-Breaker/src/game.js
+++ Brick-Breaker/src/game.js
@@ -2,6 +2,7 @@ import Paddle from "./paddle.js";
 import InputHandler from "./input.js";
 import Ball from "./ball.js";
 import Brick from "./brick.js";
+import { buildLevel, level1 } from "./levels.js";
 
 export default class Game {
   constructor(gameWidth, gameHeight) {
@@ -15,10 +16,7 @@ export default class Game {
     this.ball = new Ball(this);
     this.paddle = new Paddle(this);
 
-    let bricks = [];
-    for (let i = 0; i < 10; i++) {
-      bricks.push(new Brick(this, { x: 1 * 52, y: 30 }));
-    }
+    let bricks = buildLevel(this, level1);
 
     this.gameObjects = [this.ball, this.paddle, ...bricks];
 
