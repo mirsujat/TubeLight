6db3159144f360c17b9e296e11cd11dd37141cb9
Mir Sujat <mir.sujat@gmail.com>
2019-05-15 18:03:22 +0600

    implement collition detection

--- Brick-Breaker/src/ball.js
+++ Brick-Breaker/src/ball.js
@@ -5,6 +5,8 @@ export default class Ball {
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
 
+    this.game = game;
+
     this.position = { x: 10, y: 10 };
     this.speed = { x: 2, y: 2 };
     this.size = 16;
@@ -21,15 +23,34 @@ export default class Ball {
   }
 
   update(deltaTime) {
+    //console.log(this.game.paddle.position.x);
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
+    // check to see the ball hit the wall on left or right and
     //bounce the ball to oposite direction
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
+    // check to see the ball hit the wall on top or bottom and
+    //bounce the ball to oposite direction
     if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+    // check  the collision with paddle
+    let bottomOfBall = this.position.y + this.size;
+    let topOfPadddle = this.game.paddle.position.y;
+    let leftSideOfPaddle = this.game.paddle.position.x;
+    let rightSideOfPaddle =
+      this.game.paddle.position.x + this.game.paddle.width;
+
+    if (
+      bottomOfBall >= topOfPadddle &&
+      this.position.x >= leftSideOfPaddle &&
+      this.position.x + this.size <= rightSideOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
