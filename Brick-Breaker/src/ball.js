6a608370b1f1841e1a1a80f3419f4a3114bcb333
Mir Sujat <mir.sujat@gmail.com>
2019-05-15 14:38:12 +0600

    Update ball.js

--- Brick-Breaker/src/ball.js
+++ Brick-Breaker/src/ball.js
@@ -1,13 +1,35 @@
 export default class Ball {
-  constructor() {
+  constructor(gameWidth, gameHeight) {
     this.image = document.getElementById("img_ball");
+
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+
+    this.position = { x: 10, y: 10 };
+    this.speed = { x: 2, y: 2 };
+    this.size = 16;
   }
 
   draw(ctx) {
-    ctx.drawImage(this.image, 10, 10, 16, 16);
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.size,
+      this.size
+    );
   }
 
-  update() {
-    //todo
+  update(deltaTime) {
+    this.position.x += this.speed.x;
+    this.position.y += this.speed.y;
+
+    //bounce the ball to oposite direction
+    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+      this.speed.x = -this.speed.x;
+    }
+    if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
+      this.speed.y = -this.speed.y;
+    }
   }
 }
