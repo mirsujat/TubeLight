c463c3530d174a655ae58ca1153cfbec9b67aa7f
Mir Sujat <mir.sujat@gmail.com>
2019-07-27 21:09:44 +0600

    Update Articles.js

--- react-hooks/src/Pages/Articles.js
+++ react-hooks/src/Pages/Articles.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Articles = () => (
-  <div>
-    <h1>Hello From Articles Page</h1>
-  </div>
-);
+const Articles = () => {
+  const [posts, setPosts] = useState({ data: [] });
 
+  useEffect(() => {
+    let ignore = false;
+    const fetchPosts = async () => {
+      const posts = await axios("https://jsonplaceholder.typicode.com/posts");
+      if (!ignore) setPosts(posts);
+    };
+    fetchPosts();
+    return () => {
+      ignore = true;
+    };
+  });
+
+  return (
+    <div>
+      <h1>Hello From Articles Page</h1>
+    </div>
+  );
+};
 export default Articles;
