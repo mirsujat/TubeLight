206487d546c565726a813e06264d522e114ab775
Mir Sujat <mir.sujat@gmail.com>
2019-07-28 22:06:46 +0600

    Update Routes.js

--- react-hooks/src/Routes/Routes.js
+++ react-hooks/src/Routes/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Home from "../Pages/Home";
+import Form from "../Pages/Form";
 import Articles from "../Pages/Articles";
 import Article from "../Pages/Article";
 
@@ -10,6 +11,7 @@ const Routes = () => (
     <Navbar />
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/form" component={Form} />
       <Route path="/api/articles" component={Articles} />
       <Route path="/api/:id" component={Article} />
     </Switch>
