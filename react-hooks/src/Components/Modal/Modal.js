1431e1eedc86eb1faaee3d9abad3143b1316c0f8
Mir Sujat <mir.sujat@gmail.com>
2019-07-30 16:45:09 +0600

    create Modal

---
+++ react-hooks/src/Components/Modal/Modal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Modal.css";
+
+const Modal = ({ isShowing, hide }) =>
+  isShowing
+    ? ReactDOM.createPortal(
+        <React.Fragment>
+          <div className="modal-overlay" />
+          <div
+            className="modal-wrapper"
+            aria-modal
+            aria-hidden
+            tabIndex={-1}
+            role="dialog"
+          >
+            <div className="modal">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="modal-close-button"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={hide}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <p>Hello, I'm a modal.</p>
+            </div>
+          </div>
+        </React.Fragment>,
+        document.body
+      )
+    : null;
+
+export default Modal;
--- react-hooks/src/Components/Navbar/Navbar.js
+++ react-hooks/src/Components/Navbar/Navbar.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Modal from "../Modal/Modal";
+import useModal from "../Modal/useModal";
 
-const Navbar = () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/simple-form">Simple Form with Hooks</Link>
-    </li>
-    <li>
-      <Link to="/form">Form with No Validation</Link>
-    </li>
-    <li>
-      <Link to="/form-validation">Form with Validation</Link>
-    </li>
-    <li>
-      <Link to="/api/articles">Articles</Link>
-    </li>
-  </ul>
-);
+const Navbar = () => {
+  const { isShowing, toggle } = useModal();
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/simple-form">Simple Form with Hooks</Link>
+        </li>
+        <li>
+          <Link to="/form">Form with No Validation</Link>
+        </li>
+        <li>
+          <Link to="/form-validation">Form with Validation</Link>
+        </li>
+        <li>
+          <Link to="/api/articles">Articles</Link>
+        </li>
+        <button onClick={toggle}>Show Modal</button>
+      </ul>
+      <Modal isShowing={isShowing} hide={toggle} />
+    </div>
+  );
+};
 
 export default Navbar;
