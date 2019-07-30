2f19bbc9fda01a17032babc7fa945f6a7cf2af3f
Mir Sujat <mir.sujat@gmail.com>
2019-07-30 12:44:53 +0600

    select component added

--- react-hooks/src/Pages/SimpleForm.js
+++ react-hooks/src/Pages/SimpleForm.js
@@ -32,6 +32,24 @@ const SimpleForm = () => {
           onChange={handleChange}
         />
         <br />
+        <select
+          name="select"
+          value={values.select || "Mango"}
+          onChange={handleChange}
+        >
+          <option value={values.grapefruit}>Grapefruit</option>
+          <option value={values.lime}>Lime</option>
+          <option value={values.coconut}>Coconut</option>
+          <option value={values.mango}>Mango</option>
+        </select>
+        <br />
+        <input
+          name="isGoing"
+          type="checkbox"
+          checked={values.isGoing || false}
+          onChange={handleChange}
+        />
+        <br />
         <button type="submit">Submit</button>
       </form>
     </div>
