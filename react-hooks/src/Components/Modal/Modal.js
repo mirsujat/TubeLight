143dd41fd682e23581ee840afa57e5d9167f5b3f
Mir Sujat <mir.sujat@gmail.com>
2019-07-31 14:41:22 +0600

    refactor

--- react-hooks/src/Components/Modal/Modal.js
+++ react-hooks/src/Components/Modal/Modal.js
@@ -1,49 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./Modal.css";
+import Portal from "./Portal";
 
 const Modal = ({ isShowing, hide }) =>
-  isShowing
-    ? ReactDOM.createPortal(
-        <React.Fragment>
-          <div className="modal-overlay" onClick={hide} />
-          <div className="modal-root">
-            <div
-              className="modal-wrapper"
-              aria-modal
-              aria-hidden
-              tabIndex={-1}
-              role="dialog"
-            >
-              <div className="modal">
-                <div className="modal-header">
-                  <button
-                    type="button"
-                    className="modal-close-button"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                    onClick={hide}
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-content">
-                  <p>
-                    Hello, I'm a modal.Hello, I'm a modal.Hello, I'm a modal.{" "}
-                  </p>
-                  <p>
-                    Hello, I'm a modal.Hello, I'm a modal.Hello, I'm a modal.
-                  </p>
-                  <p>
-                    Hello, I'm a modal.Hello, I'm a modal.Hello, I'm a modal.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </React.Fragment>,
-        document.body
-      )
-    : null;
+  isShowing ? (
+    <Portal hide={hide}>
+      <p>Hello From React Portal, Modal and Hoooks</p>
+      <p>Hello From React Portal, Modal and Hoooks</p>
+      <p>Hello From React Portal, Modal and Hoooks</p>
+      <p>Hello From React Portal, Modal and Hoooks</p>
+    </Portal>
+  ) : null;
 
 export default Modal;
