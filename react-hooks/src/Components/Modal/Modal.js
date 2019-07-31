1a50f6c49164b8e6ea8ac356eb246bef9717148d
Mir Sujat <mir.sujat@gmail.com>
2019-07-31 12:04:50 +0600

    Update Modal.js

--- react-hooks/src/Components/Modal/Modal.js
+++ react-hooks/src/Components/Modal/Modal.js
@@ -7,26 +7,28 @@ const Modal = ({ isShowing, hide }) =>
     ? ReactDOM.createPortal(
         <React.Fragment>
           <div className="modal-overlay" onClick={hide} />
-          <div
-            className="modal-wrapper"
-            aria-modal
-            aria-hidden
-            tabIndex={-1}
-            role="dialog"
-          >
-            <div className="modal">
-              <div className="modal-header">
-                <button
-                  type="button"
-                  className="modal-close-button"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                  onClick={hide}
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
+          <div className="modal-root">
+            <div
+              className="modal-wrapper"
+              aria-modal
+              aria-hidden
+              tabIndex={-1}
+              role="dialog"
+            >
+              <div className="modal">
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="modal-close-button"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                    onClick={hide}
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <p>Hello, I'm a modal.</p>
               </div>
-              <p>Hello, I'm a modal.</p>
             </div>
           </div>
         </React.Fragment>,
