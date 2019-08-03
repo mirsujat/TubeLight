4c2c18ac672ca8b81d878b030b1d67addd7a5d65
Mir Sujat <mir.sujat@gmail.com>
2019-08-03 20:51:36 +0600

    debounce search input and result

--- react-hooks/src/Pages/Home.js
+++ react-hooks/src/Pages/Home.js
@@ -1,68 +1,62 @@
 import React, { useState, useEffect, useMemo } from "react";
+import useDebounce from "../Components/CustomHooks/useDebounce";
 import axios from "axios";
 
 const Home = () => {
   const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState("react");
-  const [searchTerm, setSearchTerm] = useState("");
-  const [submit, setSubmit] = useState(false);
+  const [isSearching, setIsSearching] = useState(false);
 
-  // fetch data from remote api.
-  useEffect(() => {
-    // let ignore = false;
-    const fetchData = () => {
-      axios("https://hn.algolia.com/api/v1/search?query=" + query)
-        .then(result => {
-          setData(result.data);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    };
-    fetchData();
-  }, [query]);
+  // Now we call our hook, passing in the current searchTerm value.
+  // The hook will only return the latest value (what we passed in) ...
+  // ... if it's been more than 500ms since it was last called.
+  // Otherwise, it will return the previous value of searchTerm.
+  // The goal is to only have the API call fire when user stops typing ...
+  // ... so that we aren't hitting our API rapidly.
+  const debouncedSearchTerm = useDebounce(query, 1000);
 
-  const searchHandler = () => {
-    axios("https://hn.algolia.com/api/v1/search?query=" + searchTerm)
-      .then(result => {
-        setTimeout(() => {
-          setData(result.data);
-          setSubmit(true);
-        }, 2000);
-      })
+  const fetchData = query => {
+    return axios("https://hn.algolia.com/api/v1/search?query=" + query)
+      .then(result => result)
       .catch(err => {
         console.log(err);
+        return [];
       });
   };
-
+  // fetch data from remote api.
   useEffect(() => {
-    if (submit) {
-      searchHandler();
+    // Make sure we have a value (user has entered something in input)
+    if (debouncedSearchTerm) {
+      // Set isSearching state
+      setIsSearching(true);
+      // Fire off our API call
+      fetchData(debouncedSearchTerm).then(result => {
+        // Set back to false since request finished
+        setIsSearching(false);
+        setData(result.data);
+      });
+    } else {
+      setData({ hits: [] });
     }
-  }, [submit]);
+  }, [debouncedSearchTerm]);
 
   function handleChange(e) {
-    setSearchTerm(e.target.value);
+    setQuery(e.target.value);
   }
+
   console.log("Data: ", data);
 
-  let content = <div>Loading...</div>;
+  let content = null;
 
-  if (data.hits.length > 0) {
+  if (data.hits.length === 0) {
+    content = <div>Loading...</div>;
+  }
+  if (isSearching && data.hits.length > 0) {
+    content = <div>Searching...</div>;
+  }
+  if (data.hits.length > 0 && !isSearching) {
     content = (
       <div>
-        <header>
-          <h1>React Hooks</h1>
-        </header>
-        <input
-          type="text"
-          name={searchTerm}
-          value={searchTerm}
-          onChange={handleChange}
-        />
-        <button type="button" onClick={searchHandler}>
-          submit
-        </button>
         <ul>
           {data.hits.map(item => (
             <li key={item.objectID}>
@@ -74,7 +68,18 @@ const Home = () => {
     );
   }
 
-  return <div>{content}</div>;
+  return (
+    <div>
+      <header>
+        <h1>React Hooks</h1>
+      </header>
+      <input type="text" name={query} value={query} onChange={handleChange} />
+      <button type="button" onClick={() => console.log("submitted!!")}>
+        submit
+      </button>
+      {content}
+    </div>
+  );
 };
 
 export default Home;
