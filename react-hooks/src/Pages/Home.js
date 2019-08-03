f1898723c14c4b9f22c43406b0bd9c9d88b4be00
Mir Sujat <mir.sujat@gmail.com>
2019-08-03 19:18:51 +0600

    mir

--- react-hooks/src/Pages/Home.js
+++ react-hooks/src/Pages/Home.js
@@ -1,44 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 
 const Home = () => {
   const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState("react");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [submit, setSubmit] = useState(false);
 
   // fetch data from remote api.
   useEffect(() => {
-    let ignore = false;
-
-    const fetchData = async () => {
-      const result = await axios(
-        "https://hn.algolia.com/api/v1/search?query=" + query
-      );
-      if (!ignore) setData(result.data);
+    // let ignore = false;
+    const fetchData = () => {
+      axios("https://hn.algolia.com/api/v1/search?query=" + query)
+        .then(result => {
+          setData(result.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     };
     fetchData();
-    return () => {
-      ignore = true;
-    };
   }, [query]);
 
+  const searchHandler = () => {
+    axios("https://hn.algolia.com/api/v1/search?query=" + searchTerm)
+      .then(result => {
+        setTimeout(() => {
+          setData(result.data);
+          setSubmit(true);
+        }, 2000);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    if (submit) {
+      searchHandler();
+    }
+  }, [submit]);
+
   function handleChange(e) {
-    setQuery(e.target.value);
+    setSearchTerm(e.target.value);
   }
-  return (
-    <div>
-      <header>
-        <h1>React Hooks</h1>
-      </header>
-      <input type="text" name={query} value={query} onChange={handleChange} />
-      <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  console.log("Data: ", data);
+
+  let content = <div>Loading...</div>;
+
+  if (data.hits.length > 0) {
+    content = (
+      <div>
+        <header>
+          <h1>React Hooks</h1>
+        </header>
+        <input
+          type="text"
+          name={searchTerm}
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <button type="button" onClick={searchHandler}>
+          submit
+        </button>
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  return <div>{content}</div>;
 };
 
 export default Home;
