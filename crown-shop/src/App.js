ae3112ebf08922e202aaf8a81e27f137e8321f44
Mir Sujat <mir.sujat@gmail.com>
2019-08-18 20:37:03 +0600

    update our manu-item to nagvigate withRoute HOC

--- crown-shop/src/App.js
+++ crown-shop/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- crown-shop/src/components/Directory/Directory.js
+++ crown-shop/src/components/Directory/Directory.js
@@ -43,10 +43,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => {
-          return (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-          );
+        {this.state.sections.map(({ id, ...otherSectionProps }) => {
+          return <MenuItem key={id} {...otherSectionProps} />;
         })}
       </div>
     );
