0f536f68f567096cfd93e93f89976fbf1b714645
Mir Sujat <mir.sujat@gmail.com>
2019-08-18 22:00:01 +0600

    Update App.js

--- crown-shop/src/App.js
+++ crown-shop/src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from "./pages/ShopPage/ShopPage";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
