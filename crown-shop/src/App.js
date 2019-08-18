da839e455040dd9376dc55b0bc067d6a24af8956
Mir Sujat <mir.sujat@gmail.com>
2019-08-18 19:09:06 +0600

    Finished HomePage

--- crown-shop/src/App.js
+++ crown-shop/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello From Crown Shop</h1>
+    <div>
+      <HomePage />
     </div>
   );
 }
---
+++ crown-shop/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../components/Directory/Directory";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
