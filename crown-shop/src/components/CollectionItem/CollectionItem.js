c36390fbaba439a639ab55e3fb421dd5d560b0e9
Mir Sujat <mir.sujat@gmail.com>
2019-08-19 11:50:14 +0600

    update CollectionPreview and CollectionItem Component

---
+++ crown-shop/src/components/CollectionItem/CollectionItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./collectionitem.styles.scss";
+
+const CollectionItem = ({ id, name, imageUrl, price }) => (
+  <div className="collection-item">
+    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
--- crown-shop/src/components/CollectionPreview/CollectionPreview.js
+++ crown-shop/src/components/CollectionPreview/CollectionPreview.js
@@ -1,14 +1,17 @@
 import React from "react";
+import CollectionItem from "../CollectionItem/CollectionItem";
 
 import "./collectionpreview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="prewview">
-      {items.map(item => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
