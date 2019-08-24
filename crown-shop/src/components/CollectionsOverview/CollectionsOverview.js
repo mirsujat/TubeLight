e5a4faa17ba8db45e3c34226551c730bafe699c1
Mir Sujat <mir.sujat@gmail.com>
2019-08-24 20:04:31 +0600

    Update CollectionsOverview.js

--- crown-shop/src/components/CollectionsOverview/CollectionsOverview.js
+++ crown-shop/src/components/CollectionsOverview/CollectionsOverview.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionForPreview } from "../../redux/shop/shop.selectors";
 
 import CollectionPreview from "../CollectionPreview/CollectionPreview";
 
@@ -15,6 +15,6 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 );
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionForPreview
 });
 export default connect(mapStateToProps)(CollectionsOverview);
