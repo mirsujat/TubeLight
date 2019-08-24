11ecd99ede97fb34a67496350aa9d0bc44718338
Mir Sujat <mir.sujat@gmail.com>
2019-08-24 20:04:46 +0600

    Update CollectionPage.js

--- crown-shop/src/pages/CollectionPage/CollectionPage.js
+++ crown-shop/src/pages/CollectionPage/CollectionPage.js
@@ -2,14 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/CollectionItem/CollectionItem";
 
 import "./collectionpage.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log("Mir: ", collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>CATEGORY APGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
