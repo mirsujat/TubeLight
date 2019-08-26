6f8a53f05b91e2a704e61ea821dbf08450e0aa3f
Mir Sujat <mir.sujat@gmail.com>
2019-08-26 14:59:25 +0600

    Implement addCollectionAndDocuments method to programatically add data to firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -36,6 +36,23 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+// this will save local data to firestore database
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    //will give us new document reference with unique key
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit(); // it will give us promise back
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
