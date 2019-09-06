074311796d7e2e21257c110d86f94c6a9c3a9fa0
Mir Sujat <mir.sujat@gmail.com>
2019-09-06 19:26:04 +0600

    Update SignIn.js

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -52,7 +52,11 @@ class SignIn extends Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign In With Google
             </CustomButton>
           </div>
