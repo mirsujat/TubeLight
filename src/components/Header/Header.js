59323a1614e1bbc47fafee8a9f5b3c0b242957e3
Mir Sujat <mir.sujat@gmail.com>
2019-09-07 22:21:02 +0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartIcon from "../CartIcon/CartIcon";
 import CartDropdown from "../CartDropdown/CartDropdown";
