7792b1137100bae80de349d706e00a4f7c5a145b
Mir Sujat <mir.sujat@gmail.com>
2020-06-24 11:47:24 +0600

    footer added

--- crown-shop/src/App.js
+++ crown-shop/src/App.js
@@ -6,6 +6,7 @@ import { createStructuredSelector } from "reselect";
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 import HomePage from "./pages/HomePage/HomePage";
 import ShopPage from "./pages/ShopPage/ShopPage";
@@ -21,13 +22,13 @@ class App extends Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           setCurrentUser({
             id: snapShot.id,
-            ...snapShot.data()
+            ...snapShot.data(),
           });
         });
       }
@@ -61,20 +62,18 @@ class App extends Component {
             }
           />
         </Switch>
+        <Footer></Footer>
       </div>
     );
   }
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ crown-shop/src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import gitLogo from "./Octocat.jpg";
+import "./footer.styles.scss";
+
+const Footer = () => {
+  let date = new Date().getFullYear();
+  let github = "https://github.com/mirsujat/TubeLight/tree/master/crown-shop";
+  let angelList = "https://angel.co/u/mir-sujat";
+  return (
+    <div className="footer">
+      <div className="footer-left">
+        <h6>Develop and design by mir sujat &copy; {date} </h6>
+      </div>
+      <div className="hire-me">
+        <h6>
+          <a href={angelList} target="_blank" rel="noopener noreferrer">
+            Hire Me!
+          </a>
+        </h6>
+      </div>
+      <div className="footer-right">
+        <span className="source-code">Find Source code: </span>
+        <a href={github} target="_blank" rel="noopener noreferrer">
+          <img src={gitLogo} alt="gitHub link" heigth="50px" width="50px"></img>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
