80e87bd22cd19d595c646a65cf09cb8e8cee2842
Mir Sujat <mir.sujat@gmail.com>
2020-06-24 12:11:30 +0600

    Update Footer.js

--- crown-shop/src/components/Footer/Footer.js
+++ crown-shop/src/components/Footer/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import gitLogo from "./Octocat.jpg";
 import "./footer.styles.scss";
 
@@ -10,7 +9,10 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-left">
-        <h6>Develop and design by mir sujat &copy; {date} </h6>
+        <h6>
+          Develop and design by mir sujat &copy; {date}, contact:
+          mir.sujat@gmail.com{" "}
+        </h6>
       </div>
       <div className="hire-me">
         <h6>
