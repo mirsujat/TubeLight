d23f18b65d46ce221e0c76dbeb47c67b4647b541
Mir Sujat <mir.sujat@gmail.com>
2020-07-30 11:51:44 +0600

    Create Tabs.js

---
+++ react-hooks/src/Components/Tabs/Tabs.js
@@ -0,0 +1,37 @@
+import React, { useState, Component } from "react";
+import styles from "./Tabs.module.css";
+
+const Tabs = (props) => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const activeTab = props.children[activeTabIndex];
+
+  return (
+    <div>
+      <div className={styles.tabs}>
+        {props.children.map((tab, i) => (
+          <button
+            className={styles.tab_btn}
+            onClick={() => {
+              setActiveTabIndex(i);
+            }}
+            key={i}
+          >
+            {tab.props.title}
+          </button>
+        ))}
+      </div>
+      <div className={styles.tab_indicator_container}>
+        <div
+          className={styles.tab_indicator}
+          style={{
+            width: 100 / props.children.length + "%",
+            transform: `translateX(${activeTabIndex * 100}%)`,
+          }}
+        />
+      </div>
+      <div className={styles.tab_content}>{activeTab.props.children}</div>
+    </div>
+  );
+};
+
+export default Tabs;
